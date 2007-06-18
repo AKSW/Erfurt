0fdf032927839bbe8a92ba51805270564efe62a7
Philipp Frischmuth <pfrischmuth@googlemail.com>
2007-06-18 10:40:17 +0000

    [modified] revised property class for use with erfurt [modified] set enoding to UTF-8

--- erfurt/Owl/Property.php
+++ erfurt/Owl/Property.php
@@ -3,7 +3,7 @@
  * Property.php
  * Encoding: UTF-8
  *
- * Copyright (c) 2004-2007 Sören Auer <soeren@auer.cx>, Philipp Frischmuth <philipp@frischmuth24.de>
+ * Copyright (c) 2004-2007 SÃ¶ren Auer <soeren@auer.cx>, Philipp Frischmuth <philipp@frischmuth24.de>
  *
  * This file is part of pOWL - web based ontology editor.
  *
@@ -27,178 +27,216 @@
  * a object property.
  * 
  * @package owl
- * @author Sören Auer <soeren@auer.cx>, Philipp Frischmuth <philipp@frischmuth24.de>
+ * @author SÃ¶ren Auer <soeren@auer.cx>, Philipp Frischmuth <philipp@frischmuth24.de>
  * @copyright Copyright (c) 2004-2007
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id: $
  */
-class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
+class Erfurt_Owl_Property  extends RDFSProperty {
 	
 	/**
 	 * Checks whether the domain of this property is an union class description.
 	 *
-	 * @return boolean True if the domain is an union class description, false otherwise.
+	 * @return Resource/boolean Returns the domain iff the domain is an union class description, false otherwise.
 	 */
 	public function domainIsUnionClass() {
 		
-		if($domain=$this->getDomain())
-			if($domain->listUnionOf())
-				return $domain;
+		if ($domain = $this->getDomain()) {
+			if ($domain->listUnionOf()) return $domain;
+		}
+			
 		return false;
 	}
 	
 	/**
-	 * OWLProperty::addDomainToUnionClass()
+	 * 
 	 *
-	 * @param mixed $class
-	 * @return
+	 * @param Resource $class
 	 */
 	public function addDomainToUnionClass($class) {
 		
-		if(!$domain=$this->domainIsUnionClass())
-			$this->setDomainToUnionClass(array($class));
-		else
-			$domain->setUnionOf(array_push($domain->getUnionOf(),$class));
+		if (!(domain = $this->domainIsUnionClass())) $this->setDomainToUnionClass($class);
+		else $domain->setUnionOf(array_push($domain->getUnionOf(), $class));
 	}
 	
+	/**
+	 *
+	 *
+	 * @param Resource/Resource[] $members
+	 */
 	public function setDomainToUnionClass($members) {
-		if(count($members)==1)
-			$this->setDomain($members);
+		
+		if (($members instanceof Resource) || (count($members) === 1)) $this->setDomain($members);
 		else {
-			if(!$domain=$this->domainIsUnionClass()) {
-				$domain=$this->model->addAnonymousClass();
+			if (!($domain = $this->domainIsUnionClass())) {
+				$domain = $this->model->addAnonymousClass();
 				$this->setDomain($domain);
 			}
 			$domain->setUnionOf($members);
 		}
 	}
 	
+	/**
+	 *
+	 *
+	 * @return Resource[]
+	 */
 	public function listDomainFromUnionClass() {
 		
-		if(!$domain=$this->domainIsUnionClass())
-			return $this->getDomain()?array($this->getDomain()):array();
-		else
+		if (!($domain = $this->domainIsUnionClass())) {
+			$domain = $this->getDomain();
+			return $domain ? array($domain) : array();
+		} else {
 			return $domain->listUnionOf();
+		}	
 	}
 	
 	/**
 	 * Checks whether the range of this property is an union class description.
 	 *
-	 * @return boolean True if the range is an union class description, false otherwise.
+	 * @return Resource/boolean Returns the range iff the range is an union class description, false otherwise.
 	 */
 	public function rangeIsUnionClass() {
 		
-		if($range=$this->getRange())
-			if($range->listUnionOf())
-				return $range;
+		if ($range = $this->getRange()) {
+			if ($range->listUnionOf()) return $range;
+		}
+		
 		return false;
 	}
 	
+	/**
+	 * Checks whether the range of this property is an owl:DataRange.
+	 *
+	 * @return Resource/boolean Returns the range iff the range is an owl:DataRange, false otherwise.
+	 */
 	public function rangeIsDataRange() {
 		
-		if($range=$this->getRange())
-			if($range->type($GLOBALS['OWL_DataRange']))
-				return $range;
+		if ($range = $this->getRange()) {
+			if ($range->type('owl:DataRange')) return $range;
+		}
+			
 		return false;
 	}
 	
+	/**
+	 * 
+	 *
+	 * @return RDFSResource[]
+	 */
 	public function listRangeFromDataRange() {
 		
-		if($this->rangeIsDataRange())
-			return $this->model->getOneOf($this->getRange());
-		else
-			return false;
+		if ($this->rangeIsDataRange()) return $this->model->getOneOf($this->getRange());
+		else return false;
 	}
 	
+	/**
+	 * 
+	 *
+	 * @param Resource $class
+	 */
 	public function addRangeToUnionClass($class) {
 		
-		if(!$range=$this->domainIsUnionClass())
-			$this->setRangeToUnionClass(array($class));
-		else
-			$range->setUnionOf(array_push($range->getUnionOf(),$class));
+		if(!($range = $this->domainIsUnionClass())) $this->setRangeToUnionClass($class);
+		else $range->setUnionOf(array_push($range->getUnionOf(), $class));
 	}
 	
+	/**
+	 * 
+	 *
+	 * @param Resource/Resource[] $members
+	 */
 	public function setRangeToUnionClass($members) {
 		
-		$members=array_filter($members);
-
-		if(count($members)==1 || count($members)==0)
-			$this->setRange($members);
+		if (($members instanceof Resource)) $this->setRange($range);
 		else {
-			if(!$range=$this->rangeIsUnionClass()) {
-				$range=$this->model->addAnonymousClass();
+			$members = array_filter($members);
+			
+			if ((count($members) === 1) || (count($members) === 0))	$this->setRange($members);
+			else {
+				if (!($range = $this->rangeIsUnionClass())) {
+					$range = $this->model->addAnonymousClass();
+				}
+				
 				$this->setRange($range);
+				$range->setUnionOf($members);	
 			}
-			$range->setUnionOf($members);
-			$this->setRange($range);
 		}
 	}
 	
+	/**
+	 * 
+	 *
+	 * @param Resource[] $members
+	 */
 	public function setRangeToDataRange($members) {
 		
-		$data=$this->listRangeFromDataRange();
-		if($data && $members==array_keys($data))
-			return;
+		$data = $this->listRangeFromDataRange();
+		
+		if($data && ($members == array_keys($data))) return;
+		
 		$this->removeRange();
-		$range=new Blanknode($this->model->getUniqueResourceURI(BNODE_PREFIX));
-		$this->model->add($range,$GLOBALS['RDF_type'],$GLOBALS['OWL_DataRange']);
-		$oneOf=$this->model->addList($members);
-		$this->model->add($range,$GLOBALS['OWL_oneOf'],$oneOf);
+		$range = new Blanknode($this->model->getUniqueResourceURI(EF_BNODE_PREFIX));
+		$this->model->add($range, 'rdf:type', 'owl:DataRange');
+		$oneOf = $this->model->addList($members);
+		$this->model->add($range, 'owl:oneOf', $oneOf);
 		$this->addRange($range);
 	}
 	
+	/**
+	 * 
+	 *
+	 * @return Resource[]
+	 */
 	public function listRangeFromUnionClass() {
 		
-		if($this->rangeIsDataRange())
-			return false;
-		else if(!$range=$this->rangeIsUnionClass()) {
-			$r=$this->getRange();
-			return $r?array($r->getLocalName()=>$r):array();
-		}
-		else
-			return $range->listUnionOf();
+		if ($this->rangeIsDataRange()) return false;
+		else if (!($range = $this->rangeIsUnionClass())) {
+			$range = $this->getRange();
+			return $range ? array($range->getLocalName()=>$range) : array();
+		} else return $range->listUnionOf();
 	}
 	
 	/**
 	 * Returns an array of inverse OWLProperties indexed by their local names.
 	 *
-	 * @return array An array of inverse OWLProperties indexed by their local names.
+	 * @return Erfurt_Owl_Property[] An array of inverse OWLProperties indexed by their local names.
 	 */
 	public function listInverseOf() {
-		
-		return $this->listPropertyValuesSymmetric($GLOBALS['OWL_inverseOf'],'Property');
+// TODO use a method with a better name than listPropertyValuesSymmetric
+		return $this->listPropertyValuesSymmetric('owl:inverseOf', 'property');
 	}
 	
 	/**
 	 * Returns an array of equivalent OWLProperties indexed by their local names.
 	 *
-	 * @return array An array of equivalent OWLProperties indexed by their local names.
+	 * @return Erfurt_Owl_Property[] An array of equivalent OWLProperties indexed by their local names.
 	 */
 	public function listEquivalentProperties() {
-		
-		return $this->listPropertyValuesSymmetric($GLOBALS['OWL_equivalentProperty'],'Property');
+// TODO use a method with a better name than listPropertyValuesSymmetric		
+		return $this->listPropertyValuesSymmetric('owl:equivalentProperty', 'property');
 	}
 	
 	/**
 	 * Sets inverse properties to the properties given in values.
 	 *
-	 * @param array $values An array of properties, property URIs or property local names.
+	 * @param string[]/Resource[] $values An array of properties, property URIs or property local names.
 	 * @return boolean
 	 */
 	public function setInverseOf($values) {
-		
-		return $this->setPropertyValuesSymmetric($GLOBALS['OWL_inverseOf'],$values);
+// TODO use a method with a better name than setPropertyValuesSymmetric
+		return $this->setPropertyValuesSymmetric('owl:inverseOf', $values);
 	}
 	
 	/**
 	 * Sets equivalent properties to the properties given in values.
 	 *
-	 * @param array $values An array of properties, property URIs or property local names.
+	 * @param string[]/Resource[] $values An array of properties, property URIs or property local names.
 	 * @return boolean
 	 */
 	public function setEquivalentProperties($values) {
 		
-		return $this->setPropertyValuesSymmetric($GLOBALS['OWL_equivalentProperty'],$values);
+		return $this->setPropertyValuesSymmetric('owl:equivalentProperty', $values);
 	}
 	
 	/**
@@ -209,9 +247,10 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @param boolean $bool Shall the property be set to be of type owl:FunctionalProperty - NULL - no change, TRUE - yes, FALSE - no.
 	 * @return boolean True if the property is of type owl:FunctionalProperty, false otherwise.
 	 */
-	public function isFunctional($bool=NULL) {
-		
-		return $this->type($GLOBALS['OWL_FunctionalProperty'],$bool);
+	public function isFunctional($bool = null) {
+// TODO add a setFunctional method
+// TODO revise type method
+		return $this->type('owl:FunctionalProperty', $bool);
 	}
 	
 	/**
@@ -222,9 +261,9 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @param boolean $bool Shall the property be set to be of type owl:InverseFunctionalProperty - NULL - no change, TRUE - yes, FALSE - no.
 	 * @return boolean True if the property is of type owl:InverseFunctionalProperty, false otherwise.
 	 */
-	public function isInverseFunctional($bool=NULL) {
-		
-		return $this->type($GLOBALS['OWL_InverseFunctionalProperty'],$bool);
+	public function isInverseFunctional($bool = null) {
+// TODO add a setInverseFunctional method
+		return $this->type('owl:InverseFunctionalProperty', $bool);
 	}
 	
 	/**
@@ -235,9 +274,9 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @param boolean $bool Shall the property be set to be of type owl:SymmetricProperty - NULL - no change, TRUE - yes, FALSE - no.
 	 * @return boolean True if the property is of type owl:SymmetricProperty, false otherwise.
 	 */
-	public function isSymmetric($bool=NULL) {
-		
-		return $this->type($GLOBALS['OWL_SymmetricProperty'],$bool);
+	public function isSymmetric($bool = null) {
+// TODO add a setSymmetric method		
+		return $this->type('owl:SymmetricProperty', $bool);
 	}
 	
 	/**
@@ -248,9 +287,9 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @param boolean $bool Shall the property be set to be of type owl:TransitiveProperty - NULL - no change, TRUE - yes, FALSE - no.
 	 * @return boolean True if the property is of type owl:TransitiveProperty, false otherwise.
 	 */
-	public function isTransitive($bool=NULL) {
-		
-		return $this->type($GLOBALS['OWL_TransitiveProperty'],$bool);
+	public function isTransitive($bool = null) {
+// TODO add a setTransitive method		
+		return $this->type('owl:TransitiveProperty', $bool);
 	}
 	
 	/**
@@ -261,9 +300,9 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @param boolean $bool Shall the property be set to be of type owl:AnnotationProperty - NULL - no change, TRUE - yes, FALSE - no.
 	 * @return boolean True if the property is of type owl:AnnotationProperty, false otherwise.
 	 */
-	public function isAnnotation($bool=NULL) {
-		
-		return $this->type($GLOBALS['OWL_AnnotationProperty'],$bool);
+	public function isAnnotation($bool = null) {
+// TODO add a setAnnotation method	
+		return $this->type('owl:AnnotationProperty', $bool);
 	}
 	
 	/**
@@ -274,15 +313,14 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @param boolean $bool Shall the property be set to be of type owl:DatatypeProperty - NULL - no change, TRUE - yes, FALSE - no.
 	 * @return boolean True if the property is of type owl:DatatypeProperty, false otherwise.
 	 */
-	public function isDatatypeProperty($bool=NULL) {
-		
-		if($bool!==NULL) {
-			if($bool)
-				$this->removeType($GLOBALS['OWL_ObjectProperty']);
-			else
-				$this->setType($GLOBALS['OWL_ObjectProperty']);
+	public function isDatatypeProperty($bool = null) {
+// TODO add a setDatatypeProperty method	
+		if ($bool !== null) {
+			if ($bool === true)	$this->removeType('oel:ObjectProperty');
+			else $this->setType('owl:ObjectProperty');
 		}
-		return $this->type($GLOBALS['OWL_DatatypeProperty'],$bool);
+		
+		return $this->type('owl:DatatypeProperty', $bool);
 	}
 	
 	/**
@@ -294,14 +332,13 @@ class Erfurt_Owl_Property  extends Erfurt_Rdfs_Property {
 	 * @return boolean True if the property is of type owl:ObjectProperty, false otherwise.
 	 */
 	public function isObjectProperty($bool=NULL) {
-		
-		if($bool!==NULL) {
-			if($bool)
-				$this->removeType($GLOBALS['OWL_DatatypeProperty']);
-			else
-				$this->setType($GLOBALS['OWL_DatatypeProperty']);
+// TODO add a setObjectProperty method	
+		if($bool !== null) {
+			if($bool === true) $this->removeType('owl:DatatypeProperty');
+			else $this->setType('owl:DatatypeProperty');
 		}
-		return $this->type($GLOBALS['OWL_ObjectProperty'],$bool);
+		
+		return $this->type('owl:ObjectProperty', $bool);
 	}
 }
 ?>
