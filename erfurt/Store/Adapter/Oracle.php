a415234d507df54381fe8fcafb923cee8f3f5f53
Philipp Frischmuth <pfrischmuth@googlemail.com>
2007-06-18 08:19:11 +0000

    [moved] moved adapter class for oracle backend from Rdfs package to store package, performed some renaming and minor changes

--- erfurt/Store/Adapter/Oracle.php
+++ erfurt/Store/Adapter/Oracle.php
@@ -1,13 +1,11 @@
-<?
+<?php
 /**
  * RDFSStore
  *
- * @package RDFSAPI
+ * @package rdfs
  * @author Atanas Alexandrov <sirakov@gmail.com> 
- *
  **/
-
-class POWLStore extends DefaultPOWLStore {
+class Erfurt_Store_Adapter_Oracle extends Erfurt_Store_Default {
 
 	/**
 	 * Create table and model into ORACLE
@@ -15,22 +13,20 @@ class POWLStore extends DefaultPOWLStore {
 	 * @param	string	$tablename: Table, where the data must be stored
 	 * @param	string	$modelname: Name for the new model
 	 */
-	function getNewModel($tablename, $modelname) {
+	public function getNewModel($tablename, $modelname) {
 
 		// Check if table or model already exist
- 		$tableexist_query = "SELECT count(*) FROM MDSYS.RDF_MODEL$ WHERE TABLE_NAME='".$tablename."'";
-		$modelexist_query = "SELECT count(*) FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='".$modelname."'";		
+ 		$tableexist_query = "SELECT count(*) FROM MDSYS.RDF_MODEL$ WHERE TABLE_NAME='" . $tablename . "'";
+		$modelexist_query = "SELECT count(*) FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='" . $modelname . "'";		
 
 		$trs = $dbConn->GetOne($tableexist_query); // $trs - table record set
 		$mrs = $dbConn->GetOne($modelexist_query); // $mrs - model record set
 
-		if ( $trs == 1){
+		if ($trs == 1) {
 			print '<br>'.'Table '.$tablename.' already exist, please create another table';
-		}
-		elseif ($mrs == 1){
-				print 'Model '.$tablename.' already exist, please create another model';
-		}
-		else {
+		} else if ($mrs == 1){
+			print 'Model '.$tablename.' already exist, please create another model';
+		} else {
 			$this->$dbConn->Execute("CREATE TABLE ".$tablename." (id NUMBER, triple SDO_RDF_TRIPLE_S)");
 			echo "Table created<br>";
 			$this->$dbConn->Execute("BEGIN SDO_RDF.CREATE_RDF_MODEL('".$modelname."', '".$tablename."', 'triple'); END;");
@@ -39,7 +35,6 @@ class POWLStore extends DefaultPOWLStore {
 			/**
 			 * Optionally can be created indexes, to make some operations faster
 			 */
-			
 			$this->$dbConn->Execute("CREATE INDEX ".$modelname."_sub_idx on ".$tablename."(triple.get_subject())");
 			$this->$dbConn->Execute("CREATE INDEX ".$modelname."_prop_idx on ".$tablename."(triple.get_property())");
 			$this->$dbConn->Execute("CREATE INDEX ".$modelname."_obj_idx on ".$tablename."(to_char(triple.get_object()))");
@@ -52,7 +47,8 @@ class POWLStore extends DefaultPOWLStore {
 	 * @param	string	$modelname
 	 * @param	string	$gmq: getModel query
 	 */
-	function getModel($modelname){	
+	public function getModel($modelname) {	
+		
 		$gmq = "SELECT * FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='".$modelname."'";
 		
 		$this->dbConn->SetFetchMode(ADODB_FETCH_ASSOC);
@@ -63,7 +59,7 @@ class POWLStore extends DefaultPOWLStore {
 		$_tablename = $rs->Fields(TABLE_NAME);
 		$_id = $rs->Fields(MODEL_ID);
 		
-		$arr = array($_owner,$_id,$_modelname,$_tablename);
+		$arr = array($_owner, $_id, $_modelname, $_tablename);
 		return $arr;
 		// print_r($rs->fields);	# optional	
 	}
@@ -72,13 +68,12 @@ class POWLStore extends DefaultPOWLStore {
 	 * listModels
 	 * List all models
 	 */
-	
-	function listModels(){
+	public function listModels() {
+		
 		$lmq = "SELECT MODEL_NAME FROM MDSYS.RDF_MODEL$";
 
 		$recordSet = $this->dbConn->Execute($lmq);
-		if (!$recordSet) 
-			print $this->dbConn->ErrorMsg();
+		if (!$recordSet) print $this->dbConn->ErrorMsg();
 		else {
 			$i=1;
 			while (!$recordSet->EOF) {
@@ -86,31 +81,28 @@ class POWLStore extends DefaultPOWLStore {
 				$recordSet->MoveNext();
 				$i++;
 			}
-		
 		}
 		$recordSet->Close(); # optional
-		
 	}
 	
-	function deleteModel($modelname){
+	public function deleteModel($modelname){
+		
 		/**
 		 * Check if table or model already exist
 		 * @param	string	dtq: delete table query
 		 * @param	string	dmq: delete model query
 		 */
-	 	$modelexist = "SELECT count(*) FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='".$modelname."'";
-		$dmq = "SELECT MODEL_NAME,TABLE_NAME FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='".$modelname."'";		
+	 	$modelexist = "SELECT count(*) FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='" . $modelname . "'";
+		$dmq = "SELECT MODEL_NAME,TABLE_NAME FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='" . $modelname . "'";		
 				
 		$modelExistRecordSet = $this->dbConn->GetOne($modelexist);
 		
 		$mrs = $this->dbConn->Execute($dmq)->fields[0]; // $mrs - model record set
 		$trs = $this->dbConn->Execute($dmq)->fields[1]; // $trs - table record set
 
-		if ($modelExistRecordSet==0){
+		if ($modelExistRecordSet==0) {
 			print "Table and model does not exist!";
-		}
-
-		else {		
+		} else {		
 			/**
 			 * First delete model using PL/SQL command and then drop the table
 			 */
@@ -119,6 +111,7 @@ class POWLStore extends DefaultPOWLStore {
 			$this->dbConn->Execute("DROP TABLE ".$trs);
 			print "Table deleted";
 		}
+		
 		$modelExistRecordSet->Close(); # optional
 		$mrs->Close(); # optional
 		$trs->Close(); # optional
