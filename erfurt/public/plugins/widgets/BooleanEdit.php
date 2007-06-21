6914572a87aa049ee69a48a17ca0e584922950ab
Norman Heino <norman.heino@gmail.com>
2007-06-21 18:40:47 +0000

    [added] BooleanEdit widget.

---
+++ erfurt/public/plugins/widgets/BooleanEdit.php
@@ -0,0 +1,35 @@
+<?php
+
+// require CheckboxEdit
+if (!class_exists('CheckboxEdit')) {
+	require_once ERFURT_BASE . Zend_Registry::get('config')->widgetDir . 'CheckboxEdit/CheckboxEdit.php';
+}
+
+/**
+  * Erfurt boolean edit widget
+  *
+  * @author Norman Heino <norman@feedface.de>
+  * @version $Id$
+  */
+class BooleanEdit extends CheckboxEdit {
+	
+	public function __construct($elementName = null, $selected = null, $config = array()) {
+		parent::__construct($elementName, $selected, $config);
+		
+		// support all lexical representations of xsd:boolean
+		if (in_array('true', $this->selected) || in_array('false', $this->selected)) {
+			$this->options = array('true' => 'True', 'false' => 'False');
+		} else {
+			$this->options = array('1' => 'True', '0' => 'False');
+		}
+		$this->config['cardinalityMax'] = 1;
+		$this->config['class'] = 'BooleanEditContainer';
+		$this->config['dtype'] = 'http://www.w3.org/2001/XMLSchema#boolean';
+	}
+	
+	public static function getPreferredDatatypes() {
+		return 'http://www.w3.org/2001/XMLSchema#boolean';
+	}
+}
+
+?>
