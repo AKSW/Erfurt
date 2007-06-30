fc4d7691d16a8c43e0f7efac5176081b3d5e0494
Atanas Alexandrov <sirakov@gmail.com>
2007-06-30 17:29:58 +0000

    [add] added new functions - listNamespaces, insertNamespace, RDFSModel

--- erfurt/Rdfs/oracle/model.php
+++ erfurt/Rdfs/oracle/model.php
@@ -7,40 +7,44 @@
  *
  **/
 class RDFSModel extends DefaultRDFSModel {
+	
+	function RDFSModel($store,$modelname,$type=NULL) {
+		$modelVars =& $store->dbConn->execute("SELECT * FROM MDSYS.RDF_MODEL$ WHERE MODEL_NAME='" .$modelname ."'");
+		$this->modelOwner 	= $modelVars->fields[0];
+		$this->modelID 	= $modelVars->fields[1];
+		$this->modelName 	= $modelVars->fields[2];
+		$this->tableName 	= $modelVars->fields[3];
+		parent::DefaultRDFSModel($store,$modelname,$type);
+	}
 
 	/**
 	 * Add triple to model
 	 * 
-	 * @param	string	$modelname: the name of the model, which will be used
 	 * @param	string	$subj
 	 * @param	string	$pred
 	 * @param	string	$obj
 	 * @return	boolean	$success
 	 */
-	function add($modelname, $subj, $pred, $obj){
+	public function add($subj, $pred, $obj){
 		$success = false;
-		$_getModel = new POWLStore::getModel($modelname);
-		$_owner= $_getModel->fields[0];
-		$_modelname= $_getModel->fields[2];
-		$_tablename= $_getModel->fields[3];
 		
 		//get max triple ID, generate next ID for the tripple
-		$_newTable = $_owner.".".$_tablename; // just concat -> Example ONTOWIKI.FAMILY_RDF_DATA
+		$_newTable = $this->modelOwner.".".$this->tableName; // just concat -> Example ONTOWIKI.FAMILY_RDF_DATA
 		$count = "SELECT MAX(ID) FROM ".$_newTable; // get last triple ID
-		$lastTripleId = $oracle_database->GetOne($count); // Get the number of the last tripleId
+		$lastTripleId = $this->dbConn->GetOne($count); // Get the number of the last tripleId
 		$newId = $lastTripleId + 1; // the Id for the new triple will be 1 bigger		
 		
 		//Check, if triple already exists
-		$tripleexist = "SELECT SDO_RDF.IS_TRIPLE('".$_modelname."', '".$subj."', '".$pred."', '".$obj."') AS is_triple FROM DUAL";
+		$tripleexist = "SELECT SDO_RDF.IS_TRIPLE('".$this->modelName."', '".$subj."', '".$pred."', '".$obj."') AS is_triple FROM DUAL";
 
-		if (($oracle_database->GetOne($tripleexist)) == "TRUE (EXACT)"){
+		if (($this->dbConn->GetOne($tripleexist)) == "TRUE (EXACT)"){
 			echo "Triple already exist!";
 			$success=true;
 		}
 		else {
 			//Insert triple	
-			$insert_query = "INSERT INTO ".$_tablename." ";
-			$insert_query .= "VALUES (".$newId.", SDO_RDF_TRIPLE_S('".$_modelname."','".$subj."','".$pred."','".$obj."'))";	
+			$insert_query = "INSERT INTO ".$this->tableName." ";
+			$insert_query .= "VALUES (".$newId.", SDO_RDF_TRIPLE_S('".$this->modelName."','".$subj."','".$pred."','".$obj."'))";	
 			$oracle_database->Execute($insert_query);
 		}
 		
@@ -56,32 +60,24 @@ class RDFSModel extends DefaultRDFSModel {
 	/**
 	 * Remove triple from model
 	 * 
-	 * @param	string	$modelname: the name of the model, which will be used
 	 * @param	string	$subj
 	 * @param	string	$pred
 	 * @param	string	$obj
 	 * @return	boolean	$success
 	 */
-	function remove($modelname, $subj, $pred, $obj){
+	public function remove($subj, $pred, $obj){
 		$success = false;
-		$_getModel = new POWLStore::getModel($modelname);
-		$_owner= $_getModel->fields[0];
-		$_modelname= $_getModel->fields[2];
-		$_tablename= $_getModel->fields[3];
 
-		$_newTable = $_owner.".".$_tablename; // just concat -> Example ONTOWIKI.FAMILY_RDF_DATA
-		$getLastTripleId = $oracle_database->GetOne("SELECT MAX(ID) FROM ".$_newTable);		
-
-		// query to check, if triple already exists
-		$tripleexist = "SELECT SDO_RDF.IS_TRIPLE('".$_modelname."', '".$subj."', '".$pred."', '".$obj."') AS is_triple FROM DUAL";
+		$_newTable = $this->modelOwner.".".$this->tableName; // just concat -> Example ONTOWIKI.FAMILY_RDF_DATA
+		$getLastTripleId = $this->dbConn->GetOne("SELECT MAX(ID) FROM ".$_newTable);		
+		$tripleexist = "SELECT SDO_RDF.IS_TRIPLE('".$this->modelName."', '".$subj."', '".$pred."', '".$obj."') AS is_triple FROM DUAL"; // query to check, if triple already exists
 
 		// delete triple query
 		$deleteTriple = "DELETE FROM ".$_newTable." a where a.triple.get_subject()='".$subj."' and a.triple.get_property() = '".$pred."' and to_char(a.triple.get_object()) = '".$obj."'";
 		
-		if (($oracle_database->GetOne($tripleexist)) == "TRUE (EXACT)"){
-			$oracle_database->Execute($deleteTriple);
-		}
-		else {
+		if (($this->dbConn->GetOne($tripleexist)) == "TRUE (EXACT)"){
+			$this->dbConn->Execute($deleteTriple);
+		} else {
 			print ("Error: Triple does not exist!");
 		}
 		
@@ -89,92 +85,66 @@ class RDFSModel extends DefaultRDFSModel {
 		if ($getLastTripleId > $oracle_database->GetOne("SELECT MAX(ID) FROM ".$_newTable)){
 			print ("Triple deleted successfully!");
 			$success=true;
-		}
-		else {
+		} else {
 			print ("Error: triple was not removed succesfully"); 
 		}
 		
 		return $success;	
 	}
-
+	
 	/**
-	 * Insert namespace to model
-	 * 
-	 * @param	string	$modelname
+	 * Insert new namespace into MSYS.RDF_NAMESPACE Table$
 	 * @param	string	$namespace
 	 */
-	function insertNamespace($modelname,$namespace){
-
+	public function insertNamespace($namespace){
+		$success=false;
+		$nsExist = "SELECT count(*) FROM MDSYS.RDF_NAMESPACE$ ns WHERE ns.NAMESPACE_NAME.getUrl()='".$namespace."'";	
+		$counter = $this->dbConn->GetOne($nsExist);
+		if ($counter > 0) {
+				echo "Namespace already exist";
+				$success;
+			}	else { 
+				$oracle_database->Execute("BEGIN SDO_RDF.ADD_NAMESPACES('".$namespace."'); END;");
+				print "Namespace was added successfull!";
+				$success=true;
+			}
+		return $success;
 	}
-	
-	/**
-	 * List all
+
+	 /**
+	 * Returns an array of all XML namespaces (unique)
+	 * used in the model (not the namespaces from the namespace table; 
+	 * just namespaces that are used in statements).
+	 *
+	 * @return string[] Array of XML namespaces.
 	 */
-	function listNamespaces(){
 	
+	public function listNamespaces(){
+		$query = "SELECT DISTINCT a.VALUE_NAME.getUrl() FROM MDSYS.RDF_VALUE$ a, MDSYS.RDFM_".$this->modelName." b WHERE a.VALUE_ID = b.START_NODE_ID OR a.VALUE_ID = b.END_NODE_ID OR a.VALUE_ID = b.P_VALUE_ID";
+		
+		// temp array
+		$tmpArray = array();
+		$rs = $this->dbConn->Execute($query);
+		if ($rs){
+			$i = 0; //counter for the new array keys
+			 while ($arr = $rs->FetchRow()) {
+				 if (strpos($arr[0],"#") == FALSE) {
+					  // substring from position first character to last "/"
+					$newValue = substr($arr[0],0,strrpos($arr[0],'/'));
+					$tmpArray[$i] = $newValue;
+				 }
+				 else{
+					 // substring from fist character to "#" 
+					$newValue = substr($arr[0],0,strrpos($arr[0],'#'));
+					$tmpArray[$i] = $newValue;
+				 }
+			 $i++; 
+			 }
+		}
+		
+		// put unique namespaces into new array
+		$arrNamespaces = array_unique($tmpArray); 
+		sort($arrNamespaces);
+		return $arrNamespaces;
 	}
-	
-	/**
-	 * Search for triples
-	 */
-	function find(){
-
-	}
- 	
-	/**
-	 * find Subject
-	 */	
-	function findSubjects(){
-	
-	}
-	
-	/**
-         * find Redicates
-         */
-	function findPredicates(){
-	
-	}
-
-	/**
-         * find Objects
-         */
-	function findObjects(){
-	
-	}
-	
-	/**
-         * Create rulebase
-         */
-	function createRulebase(){
-	
-	}
-	
-	/**
-         * Drop rulebase
-         */
-	function dropRulebase(){
-	
-	}
-	
-	/**
-         * Create Ruleindex
-         */
-	function createRuleindex(){
-	
-	}
-	
-	/**
-         * Drop ruleindex
-         */
-	function dropRuleindex(){
-	
-	}
-	
-	/**
-         * Insert rule
-         */
-	function insertRule(){
-	
-	}
-	
 ?>
