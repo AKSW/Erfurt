55d1c5e8ce11f3c9cb9861dcafbd2105db3a668a
Sebastian Hellmann <kurzum@googlemail.com>
2007-07-06 16:04:41 +0000

    reasoning api, only tested with powl, but not with erfurt I hope it is zended enough to fit in, but I'm not sure about it. especially the pear directory has not been zended. DIG Reasoner must be running at http://localhost:8081

---
+++ erfurt/Reasoning/ReasoningTasks.php
@@ -0,0 +1,50 @@
+<?php
+//  Interface ReasoningTasks mit Methoden subsumption, instanceCheck,
+//   retrieval...
+
+
+
+
+interface Erfurt_Reasoning_ReasoningTasks {
+
+
+// Retrieval
+public function allConceptNames ();
+public function allRoleNames ();
+public function allIndividuals ();
+
+
+// Satisfiability
+
+public function satisfiable ($concept);
+public function subsumes ($concept1, $concept2);
+public function disjoint ($concept1, $concept2);
+
+// Concept Hierarchy
+
+public function parents ($concept);
+public function children ($concept);
+public function ancestors ($concept);
+public function descendants ($concept);
+public function equivalents ($concept);
+
+//Role Hierarchy
+
+public function rparents ($role);
+public function rchildren ($role);
+public function rancestors ($role);
+public function rdescendants($role);
+
+// Individual Queries
+
+public function instances ($concept);
+public function types ($instance);
+public function instance ($instance, $concept);
+public function roleFillers ($instance, $role);
+public function relatedIndividuals ($role);
+public function toldvalues ($instance, $atom);
+
+
+
+}
+?>
