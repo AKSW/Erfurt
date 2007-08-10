7b58a7b29831f4f50128ac9721a246322c664e30
Norman Heino <norman.heino@gmail.com>
2007-08-10 13:30:04 +0000

    [removed] leftover debugging output

--- erfurt/public/plugins/widgets/ResourceEdit/resource_edit.js
+++ erfurt/public/plugins/widgets/ResourceEdit/resource_edit.js
@@ -2,5 +2,4 @@ function getAutocompleter(id) {
 	var modelHidden = $('model-' + id.substring(0, id.length - 1));
 	var url = ow.erfurtpublicbase + 'plugins/widgets/ResourceEdit/search.php?modelUri=' + encodeURIComponent(modelHidden.value);
 	new Ajax.Autocompleter('value-' + id, 'autocomplete-choices-' + id, url, {paramName: 'searchText', minChars: 3});
-	alert(url);
 }
