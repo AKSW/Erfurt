fd7adb37de61915cb8d3ca1085c55b60838bf88c
Rolland Brunec <rollxx@gmail.com>
2007-08-19 13:40:28 +0000

    rename of generator

---
+++ erfurt/Syntax/Manchester/generateParser.php
@@ -0,0 +1,8 @@
+<?php
+
+include '../../../PEAR/PHP/ParserGenerator.php';
+$a=new PHP_ParserGenerator;
+//$_SERVER['argv'] = array('lemon', '-s', 'Parser.y');
+$_SERVER['argv'] = array('lemon', '-s', './manchester.y');
+$a->main();
+?>
