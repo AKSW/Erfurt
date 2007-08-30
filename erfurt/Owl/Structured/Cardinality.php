c4c8973c8ad2aab00619f3331423eaa112d1194a
Rolland Brunec <rollxx@gmail.com>
2007-08-30 06:46:22 +0000

    added toManchesterSyntaxString

--- erfurt/Owl/Structured/Cardinality.php
+++ erfurt/Owl/Structured/Cardinality.php
@@ -4,6 +4,10 @@ class Erfurt_Owl_Structured_Cardinality
 extends Erfurt_Owl_Structured_CardinalityBase
 {
 
+	public function toManchesterSyntaxString()
+	{
+		return $this->getOnProperty() . " exactly " . $this->getCardinality();
+	}
 
 
 }
