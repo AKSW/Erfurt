befc7ec2eadecd2db74b4a3a76bc200e5bee9a14
Rolland Brunec <rollxx@gmail.com>
2007-09-17 20:44:52 +0000

    typo corrected

--- erfurt/Syntax/Manchester/generateParser.php
+++ erfurt/Syntax/Manchester/generateParser.php
@@ -1,7 +1,7 @@
 <?php
 
 include '../../lib/PEAR/PHP/ParserGenerator.php';
-$a=new PHP_ParserGenerator;
+$a=new PHP_ParserGenerator();
 //$_SERVER['argv'] = array('lemon', '-s', 'Parser.y');
 $_SERVER['argv'] = array('lemon', '-s', './manchester.y');
 $a->main();
