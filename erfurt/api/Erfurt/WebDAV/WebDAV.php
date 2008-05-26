216b3c31b7e60cfb7e576ac64b38f8e7b860b4b5
Norman Heino <norman.heino@gmail.com>
2008-05-26 15:09:31 +0000

    noch mehr Axt im Walde ;)

--- erfurt/api/Erfurt/WebDAV/WebDAV.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../erfurt.php';
-
-$WebDAV = new Erfurt_WebDAV_Server_Default($config);
-
-$WebDAV->Serverequest();
-
-?>
--- erfurt/api/Erfurt/public/plugins/widgets/ResourceEdit/resource_edit.js
+++
@@ -1,7 +0,0 @@
-function getAutocompleter(id) {
-	var modelHidden = $('model-' + id.substring(0, id.length - 1));
-	if (modelHidden) {
-		var url = erfurtPublicUri + 'plugins/widgets/ResourceEdit/search.php?modelUri=' + encodeURIComponent(modelHidden.value);
-		new Ajax.Autocompleter('value-' + id, 'autocomplete-choices-' + id, url, {paramName: 'searchText', minChars: 3});
-	}
-}
