897ab631b10b8d90b8de2d53603f5a7c52b82b88
Norman Heino <norman.heino@gmail.com>
2008-12-09 11:10:00 +0000

    added access control exception

---
+++ Erfurt/Ac/Exception.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'Erfurt/Exception.php';
+
+/**
+ * OntoWiki controller exception base class
+ *
+ * @package    erfurt
+ * @subpackage ac
+ * @copyright  Copyright (c) 2008, {@link http://aksw.org AKSW}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @version    $Id$
+ */
+class Erfurt_Ac_Exception extends Erfurt_Exception {}
