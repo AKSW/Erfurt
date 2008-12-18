42f7f8760958fc841a0a7e284c12f4dde3ad8b6d
Norman Heino <norman.heino@gmail.com>
2008-12-18 17:53:23 +0000

    comment fixed

--- Erfurt/Store/Adapter/Virtuoso.php
+++ Erfurt/Store/Adapter/Virtuoso.php
@@ -153,6 +153,7 @@ class Erfurt_Store_Adapter_Virtuoso implements Erfurt_Store_Adapter_Interface, E
 	    $this->_execSparql($insertSparql);
 	}
 	
+	/** @see Erfurt_Store_Adapter_Interface */
 	public function countWhereMatches($graphIris, $countSpec, $whereSpec)
 	{
 	    $query = new Erfurt_Sparql_SimpleQuery();
