2749eb58ce885c9fa8fd0e8b55a8819dedf695d7
Michael Martin <martin@informatik.uni-leipzig.de>
2009-03-09 13:11:05 +0000

    some tests for the escaping Problem

--- tests/Erfurt/Store/Adapter/VirtuosoTest.php
+++ tests/Erfurt/Store/Adapter/VirtuosoTest.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'test_base.php';
 require_once 'Erfurt/Store/Adapter/Virtuoso.php';
 
@@ -13,6 +14,7 @@ class Erfurt_Store_Adapter_VirtuosoTest extends PHPUnit_Framework_TestCase
     
     public function setUp()
     {
+
         $this->fixture = new Erfurt_Store_Adapter_Virtuoso($this->_options);
     }
     
@@ -26,49 +28,39 @@ class Erfurt_Store_Adapter_VirtuosoTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(true, in_array('RDF_QUAD', $this->fixture->listTables()));
     }
 
-    public function testEscapeLiterals()
-    {
-        $literal = "\n";
-        $expected = '\\\n';
-        $this->assertEquals($expected ,$this->fixture->escapeLiteral($literal));
 
-        $literal = "\r";
-        $expected = '\\\r';
-        $this->assertEquals($expected ,$this->fixture->escapeLiteral($literal));
-
-        $literal = "'";
-        $expected = "\'";
-        $this->assertEquals($expected ,$this->fixture->escapeLiteral($literal));
-
-        $literal = '"';
-        $expected = '\"';
-        $this->assertEquals($expected ,$this->fixture->escapeLiteral($literal));
-
-        $literal = '0';
-        $expected = 'false';
-        $this->assertEquals($expected ,$this->fixture->escapeLiteral($literal, "http://www.w3.org/2001/XMLSchema#boolean"));
-
-        $literal = '1';
-        $expected = 'true';
-        $this->assertEquals($expected ,$this->fixture->escapeLiteral($literal, "http://www.w3.org/2001/XMLSchema#boolean"));
-    }
-
-    public function testAddStatements()
+    public function testAddStatement()
     {
         $graphIri = "http://phpUnitTest.de/" ;
         $subject = 'http://phpUnitTest.de/LiteralTest';
         $predicate = 'http://phpUnitTest.de/escapeTheLiteral';
-        $object = "Testing \n ";
+
+        $object = "Testing \n";
         $options = array("object_type" => 1 );
-        
         $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
 
-        $graphIri = "http://phpUnitTest.de/" ;
-        $subject = 'http://phpUnitTest.de/LiteralTest';
-        $predicate = 'http://phpUnitTest.de/escapeTheLiteral';
-        $object = "Testing \r ";
+        $object = "Testing \r";
         $options = array("object_type" => 1 );
-        
+        $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
+
+        $object = 'Testing "';
+        $options = array("object_type" => 1 );
+        $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
+
+        $object = "Testing '";
+        $options = array("object_type" => 1 );
+        $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
+
+        $object = 'Testing \"';
+        $options = array("object_type" => 1 );
+        $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
+
+        $object = "Testing \"";
+        $options = array("object_type" => 1 );
+        $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
+
+        $object = "Testing \'";
+        $options = array("object_type" => 1 );
         $this->assertEquals(NULL , $this->fixture->addStatement($graphIri, $subject, $predicate, $object, $options));
 
     }
@@ -80,15 +72,31 @@ class Erfurt_Store_Adapter_VirtuosoTest extends PHPUnit_Framework_TestCase
         $predicate = 'http://phpUnitTest.de/escapeTheLiteralString';
         $statementsArray[$subject][$predicate][0]['type'] = "literal" ;
         $statementsArray[$subject][$predicate][0]['datatype'] = "http://www.w3.org/2001/XMLSchema#string";
-        $statementsArray[$subject][$predicate][0]['value'] = "Test \n \r ";
- 
-        $graphIri = "http://phpUnitTest.de/" ;
-        $subject = 'http://phpUnitTest.de/LiteralTest';
-        $predicate = 'http://phpUnitTest.de/escapeTheLiteralBoolean';
-        $statementsArray[$subject][$predicate][0]['type'] = "literal" ;
-        $statementsArray[$subject][$predicate][0]['datatype'] = "http://www.w3.org/2001/XMLSchema#boolean";
-        $statementsArray[$subject][$predicate][0]['value'] = "0";
+        #$statementsArray[$subject][$predicate][0]['value'] = "-2015454240";
+        #$statementsArray[$subject][$predicate][0]['value'] = "Museum";
+        $statementsArray[$subject][$predicate][0]['value'] = 'verhuisd onder ? dak, nu "Apeldoorns Museum"';
+        #$statementsArray[$subject][$predicate][0]['value'] = "per persoon: 113.45 &euro;<br/>CJP: 0 &euro;";
+
+        #$subject = 'http://phpUnitTest.de/LiteralTest';
+        #$predicate = 'http://phpUnitTest.de/escapeTheLiteralBoolean';
+        #$statementsArray[$subject][$predicate][0]['type'] = "literal" ;
+        #$statementsArray[$subject][$predicate][0]['datatype'] = "http://www.w3.org/2001/XMLSchema#boolean";
+        #$statementsArray[$subject][$predicate][0]['value'] = "0";
         $this->fixture->addMultipleStatements($graphIri, $statementsArray);
+
+
+
+
+#"""Museum"""
+#"""verhuisd onder ? dak, nu \"Apeldoorns Museum\""""
+#"""Een huis vol spraakmakende, vaak een beetje tegendraadse hedendaagse beeldende kunst. Wisselende exposities.""". 
+#"""per persoon: 113.45 &euro;<br/>CJP: 0 &euro;"""
+#"""Van Reekum Museum"""
+#"""van.reekum"""
+#"""http://www.apeldoorn.org/vanreekum/"""
+#"""Van Reekum Museum"""@nl
+#"""true"""^^<http://www.w3.org/2001/XMLSchema#boolean>
+
 
     }
 
