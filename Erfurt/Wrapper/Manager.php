e3cb8354335dc24f4ece15437ef7c94fdd05ede2
Philipp Frischmuth <pfrischmuth@googlemail.com>
2009-06-15 14:50:37 +0000

    [modified] doc changes, reset method for registry singleton

--- Erfurt/Wrapper/Manager.php
+++ Erfurt/Wrapper/Manager.php
@@ -5,12 +5,12 @@ require_once 'Erfurt/Wrapper/Registry.php';
  * This class provides functionality in order to scan directories for wrapper
  * plugins.
  * 
- * @package  erfurt
- * @subpackage   wrapper
- * @author    Philipp Frischmuth <pfrischmuth@googlemail.com>
- * @copyright Copyright (c) 2009 {@link http://aksw.org aksw}
- * @license   http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
- * @version   $Id$
+ * @package    erfurt
+ * @subpackage wrapper
+ * @author     Philipp Frischmuth <pfrischmuth@googlemail.com>
+ * @copyright  Copyright (c) 2009 {@link http://aksw.org aksw}
+ * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @version    $Id$
  */
 class Erfurt_Wrapper_Manager
 {
