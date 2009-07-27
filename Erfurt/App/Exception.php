115f9224121ea0be854e6e7821f23477903b4e7f
Philipp Frischmuth <pfrischmuth@googlemail.com>
2009-07-27 10:19:53 +0000

    [added] new app exception

---
+++ Erfurt/App/Exception.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'Erfurt/Exception.php';
+
+/**
+ * @package erfurt
+ * @subpackage app
+ * @author Philipp Frischmuth <pfrischmuth@googlemail.com>
+ */
+class Erfurt_App_Exception extends Erfurt_Exception
+{
+}
