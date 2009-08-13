0bf468b205d60bc05f03d114fea60c937c2ad98e
Philipp Frischmuth <pfrischmuth@googlemail.com>
2009-08-13 14:52:51 +0000

    [modified] propset, doc

--- Erfurt/Event/Dispatcher.php
+++ Erfurt/Event/Dispatcher.php
@@ -1,17 +1,25 @@
 <?php
+/**
+ * This file is part of the {@link http://aksw.org/Projects/Erfurt Erfurt} project.
+ *
+ * @copyright Copyright (c) 2008, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @version $Id$
+ */
 
 require_once 'Erfurt/Event.php';
 
 /**
  * Erfurt event dispatcher.
  *
- * @package erfurt
- * @subpackage    event
- * @author     Michael Haschke
- * @author     Norman Heino <norman.heino@gmail.com>
  * @copyright  Copyright (c) 2008, {@link http://aksw.org AKSW}
  * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ * @package    erfurt
+ * @subpackage event
  * @version    $$
+ * @author     Michael Haschke
+ * @author     Norman Heino <norman.heino@gmail.com>
+ * @author     Philipp Frischmuth <pfrischmuth@googlemail.com>
  */
 class Erfurt_Event_Dispatcher
 {
