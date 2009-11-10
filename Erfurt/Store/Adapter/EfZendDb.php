aaf61d56bf4a6450e83eaca292eb2565a9f1b628
Philipp Frischmuth <pfrischmuth@googlemail.com>
2009-11-10 10:48:50 +0100

    Fixes missing baseUri parameter issue, for baseUri is currently not supported by new createModel method.

--- Erfurt/Store/Adapter/EfZendDb.php
+++ Erfurt/Store/Adapter/EfZendDb.php
@@ -372,6 +372,7 @@ class Erfurt_Store_Adapter_EfZendDb implements Erfurt_Store_Adapter_Interface, E
             'uri'  => &$graphUri
         );
         
+        $baseUri = $graphUri;
         if ($baseUri !== '') {
             $data['base'] = $baseUri;
         }
