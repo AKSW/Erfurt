0bd40266356eb78e12a46ec932e61f40a0075cf4
Jonas Brekle <jonas.brekle@gmail.com>
2010-03-19 18:05:27 +0100

    test for query2 triple

--- tests/Erfurt/Sparql/TestSuite.php
+++ tests/Erfurt/Sparql/TestSuite.php
@@ -15,6 +15,7 @@ class Erfurt_Sparql_TestSuite extends PHPUnit_Framework_TestSuite
         $suite->addTestSuite('Erfurt_Sparql_Query2_VarTest');
         $suite->addTestSuite('Erfurt_Sparql_Query2_ContainerHelperTest');
         $suite->addTestSuite('Erfurt_Sparql_Query2_ElementHelperTest');
+        $suite->addTestSuite('Erfurt_Sparql_Query2_TripleTest');
         $suite->addTestSuite('Erfurt_Sparql_Query2_GroupGraphPatternTest');
 
         $suite->addTestSuite('Erfurt_Sparql_EngineDb_ResultRenderer_ExtendedTest');
