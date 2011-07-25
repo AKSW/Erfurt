d39e55fbbd4c1cbe0295f1cb1468d9f47d21f2a8
Jonas Brekle <jonas.brekle@gmail.com>
2011-07-25 15:46:23 +0200

    var name extraction is now sparql grammar compliant

--- Erfurt/Sparql/Query2/Var.php
+++ Erfurt/Sparql/Query2/Var.php
@@ -118,8 +118,20 @@ class Erfurt_Sparql_Query2_Var extends Erfurt_Sparql_Query2_ElementHelper implem
         
         $lower = strtolower($ret);
         
-        $notAllowed = array('-');
-        $clean = str_replace($notAllowed, '', $lower);
+        $PN_CHARS_BASE = 'A-Za-z\x{00C0}-\x{00D6}\x{00D8}-\x{00F6}\x{00F8}-\x{02FF}\x{0370}-\x{037D}\x{037F}-\x{1FFF}\x{200C}-\x{200D}\x{2070}-\x{218F}\x{2C00}-\x{2FEF}\x{3001}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFFD}\x{10000}-\x{EFFFF}';
+        $PN_CHARS_U	  = $PN_CHARS_BASE . '_';
+        $firstVarNameChar = $PN_CHARS_U.'0-9';
+        $tailVarNameChars = $PN_CHARS_U.'0-9\x{00B7}\x{0300}-\x{036F}\x{203F}-\x{2040}';
+       
+        $firstVarNameCharRegex = '/[^'.$firstVarNameChar.']/u';
+        $firstChar = substr($lower, 0, 1);
+        $firstCharClean = preg_replace($firstVarNameCharRegex, '', $firstChar);
+        
+        $tailVarNameCharsRegex = '/[^'.$tailVarNameChars.']+/u';
+        $tailChars = substr($lower, 1);
+        $tailCharsClean = preg_replace($tailVarNameCharsRegex, '', $tailChars);
+        
+        $clean = $firstCharClean.$tailCharsClean;
         
         return $clean;
     }
