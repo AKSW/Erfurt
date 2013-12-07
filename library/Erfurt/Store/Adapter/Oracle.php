a963c525f53b76c4bd852414c63202f3697b469a
Matthias Molitor <matthias@matthimatiker.de>
2013-12-07 13:56:31 +0100

    created class stub for the adapter (#13)

---
+++ library/Erfurt/Store/Adapter/Oracle.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Adapter that connects to the Oracle Triple Store (named Semantic and Graph option).
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 07.12.13
+ */
+class Erfurt_Store_Adapter_Oracle
+{
+
+}
