8629eeb919aa028142613061633b478d2646197e
Matthias Molitor <matthias@matthimatiker.de>
2013-12-12 17:17:27 +0100

    added test case stub (#13)

---
+++ tests/unit/Erfurt/Store/Adapter/OracleTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Tests the Oracle adapter factory.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 12.12.13
+ */
+class Erfurt_Store_Adapter_OracleTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsFactoryInterface()
+    {
+
+    }
+
+    public function testCreateFromOptionsThrowsExceptionIfConnectionOptionsAreOmitted()
+    {
+
+    }
+
+    public function testCreateFromOptionsThrowsExceptionIfInvalidConnectionOptionsArePassed()
+    {
+
+    }
+
+    public function testCreateFromOptionsReturnsAdapterInstanceIfOptionsAreValid()
+    {
+
+    }
+
+}
