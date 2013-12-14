a4b563cf0dd34e42aadcb4ba7fb421943d3f0206
Matthias Molitor <matthias@matthimatiker.de>
2013-12-14 22:32:10 +0100

    added stub for class that is responsible for initializing the store (#13)

---
+++ library/Erfurt/Store/Adapter/Oracle/Setup.php
@@ -0,0 +1,34 @@
+<?php
+
+use Doctrine\DBAL\Driver\Connection;
+
+/**
+ * Used to setup the Oracle Triple Store.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 14.12.13
+ */
+class Setup
+{
+
+    public function __construct(Connection $connection)
+    {
+
+    }
+
+    public function isInstalled()
+    {
+
+    }
+
+    public function install()
+    {
+
+    }
+
+    public function uninstall()
+    {
+
+    }
+
+}
