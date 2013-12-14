7138e6b8449239bd8a8af0e4f0be090186ba433a
Matthias Molitor <matthias@matthimatiker.de>
2013-12-14 13:52:09 +0100

    added test case stub (#13)

---
+++ tests/integration/Erfurt/Store/Adapter/Oracle/OracleAdapterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Tests the Oracle adapter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 14.12.13
+ */
+class Erfurt_Store_Adapter_Oracle_OracleAdapterTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testSparqlQueryReturnsArrayIfSelectQueryIsPassed()
+    {
+
+    }
+
+    public function testSparqlQueryResultContainsRequestedVariables()
+    {
+
+    }
+
+    public function testSparqlQueryResultContainsAliasedVariables()
+    {
+
+    }
+
+    public function testSparqlQueryResultIsEmptyIfNoDataMatches()
+    {
+
+    }
+
+    public function testSparqlQueryResultReturnsCorrectNumberOfRows()
+    {
+
+    }
+
+    public function testSparqlQueryResultIsOrderedCorrectly()
+    {
+
+    }
+
+}
