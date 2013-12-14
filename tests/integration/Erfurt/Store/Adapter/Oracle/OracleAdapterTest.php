9a0b2bf4347238cbbf9c2d6fe689ecf2846d084b
Matthias Molitor <matthias@matthimatiker.de>
2013-12-14 20:57:12 +0100

    added comments (#13)

--- tests/integration/Erfurt/Store/Adapter/Oracle/OracleAdapterTest.php
+++ tests/integration/Erfurt/Store/Adapter/Oracle/OracleAdapterTest.php
@@ -9,36 +9,63 @@
 class Erfurt_Store_Adapter_Oracle_OracleAdapterTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Ensures that an exception is thrown if a syntactically invalid
+     * SPARQL query is passed to sparqlQuery().
+     */
     public function testSparqlQueryThrowsExceptionIfInvalidQueryIsPassed()
     {
-        
+
     }
 
+    /**
+     * Ensures that sparqlQuery() returns an array if a select query is passed.
+     */
     public function testSparqlQueryReturnsArrayIfSelectQueryIsPassed()
     {
 
     }
 
+    /**
+     * Checks if the result set that is returned by sparqlQuery() contains
+     * the requested variables.
+     */
     public function testSparqlQueryResultContainsRequestedVariables()
     {
 
     }
 
+    /**
+     * Checks if the result set that is returned by sparqlQuery() contains
+     * the defined aliased variables.
+     */
     public function testSparqlQueryResultContainsAliasedVariables()
     {
 
     }
 
+    /**
+     * Ensures that sparqlQuery() returns an empty set if no data
+     * matches the query.
+     */
     public function testSparqlQueryResultIsEmptyIfNoDataMatches()
     {
 
     }
 
+    /**
+     * Checks if sparqlQuery() returns the correct number of rows
+     * for a query that selects a subset of the data.
+     */
     public function testSparqlQueryResultReturnsCorrectNumberOfRows()
     {
 
     }
 
+    /**
+     * Ensures that the result set that is returned by sparqlQuery()
+     * is ordered correctly.
+     */
     public function testSparqlQueryResultIsOrderedCorrectly()
     {
 
