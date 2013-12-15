19f156ff2ef615a1b2f7b1fe92a61671cb3141a2
Matthias Molitor <matthias@matthimatiker.de>
2013-12-15 09:29:49 +0100

    added comments (#13)

--- library/Erfurt/Store/Adapter/Oracle/Setup.php
+++ library/Erfurt/Store/Adapter/Oracle/Setup.php
@@ -11,21 +11,40 @@ use Doctrine\DBAL\Driver\Connection;
 class Erfurt_Store_Adapter_Oracle_Setup
 {
 
+    /**
+     * Creates a setup object that uses the provided connection to
+     * install a Triple Store.
+     *
+     * @param Connection $connection
+     */
     public function __construct(Connection $connection)
     {
 
     }
 
+    /**
+     * Checks if the Triple Store is already installed.
+     *
+     * @return boolean
+     */
     public function isInstalled()
     {
 
     }
 
+    /**
+     * Installs the Triple Store.
+     */
     public function install()
     {
 
     }
 
+    /**
+     * Removes a previously installed Triple Store.
+     *
+     * All stored data will be lost.
+     */
     public function uninstall()
     {
 
