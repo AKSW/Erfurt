f21ab1c6bc0459bf08cf22c3a4d16f6ca64f32f8
Matthias Molitor <matthias@matthimatiker.de>
2013-12-29 14:46:51 +0100

    added tests that need a database to the Oracle group (#13)

--- tests/integration/Erfurt/Store/Adapter/Oracle/OracleAdapterTest.php
+++ tests/integration/Erfurt/Store/Adapter/Oracle/OracleAdapterTest.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Connection;
  *
  * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
  * @since 14.12.13
+ * @group Oracle
  */
 class Erfurt_Store_Adapter_Oracle_OracleAdapterTest extends \PHPUnit_Framework_TestCase
 {
