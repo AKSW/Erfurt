d269203e3c377bad9531844a2b597af7f48fa5b1
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 15:26:11 +0100

    added comments (#13)

--- tests/unit/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToExtendedConverterTest.php
+++ tests/unit/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToExtendedConverterTest.php
@@ -9,36 +9,60 @@
 class Erfurt_Store_Adapter_Oracle_ResultConverter_RawToExtendedConverterTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if the converter implements the necessary interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Ensures that convert() throws an exception if no array is passed for conversion.
+     */
     public function testConvertThrowsExceptionIfNoArrayIsPassed()
     {
 
     }
 
+    /**
+     * Checks if convert() adds the variable names from the raw result set to the
+     * head section of the extended result.
+     */
     public function testConvertAddsVariablesToHeadSection()
     {
 
     }
 
+    /**
+     * Checks if convert() adds the correct number of bindings.
+     */
     public function testConvertAddsCorrectNumberOfBindings()
     {
 
     }
 
+    /**
+     * Checks if convert() adds the correct variable types to the extended
+     * result set.
+     */
     public function testConvertAddsCorrectVariableTypesToBindings()
     {
 
     }
 
+    /**
+     * Ensures that convert() assigns the correct values to the bound variables
+     * in the extended result set.
+     */
     public function testConvertAssignsCorrectVariableValues()
     {
-        
+
     }
 
+    /**
+     * Checks if convert() can handle an empty input array.
+     */
     public function testConvertCanHandleEmptyArrays()
     {
 
