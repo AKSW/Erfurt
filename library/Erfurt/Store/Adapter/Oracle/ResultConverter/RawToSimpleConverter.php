179144077c91ddb0cb9cf7330208f47b052893ce
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 15:15:43 +0100

    implemented simple result converter (#13)

--- library/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToSimpleConverter.php
+++ library/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToSimpleConverter.php
@@ -17,10 +17,24 @@ class Erfurt_Store_Adapter_Oracle_ResultConverter_RawToSimpleConverter
      *
      * @param array(array(string=>string))|mixed $resultSet
      * @return array(array(string=>string))|mixed
+     * @throws \Erfurt_Store_Adapter_ResultConverter_Exception If no array is passed.
      */
     public function convert($resultSet)
     {
-        // TODO: Implement convert() method.
+        if (!is_array($resultSet)) {
+            $message = 'Expected array for conversion.';
+            throw new Erfurt_Store_Adapter_ResultConverter_Exception($message);
+        }
+        return array_map(function (array $row) {
+            foreach (array_keys($row) as $key) {
+                // Remove additional columns that contain meta data,
+                // which is provided by Oracle.
+                if (strpos($key, '$') !== false) {
+                    unset($row[$key]);
+                }
+            }
+            return array_change_key_case($row, CASE_LOWER);
+        }, $resultSet);
     }
 
 }
