743611cbe2bc8392d990a0e40bfc50a81aec1b83
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 14:40:42 +0100

    added test case stub (#13)

---
+++ tests/unit/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToSimpleConverterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Tests the RawToSimple result converter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.12.13
+ */
+class Erfurt_Store_Adapter_Oracle_RawToSimpleConverterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterface()
+    {
+
+    }
+
+    public function testConvertThrowsExceptionIfNoArrayIsPassed()
+    {
+
+    }
+
+    public function testConvertChangesVariablesToLowerCase()
+    {
+
+    }
+
+    public function testConvertRemovesUnnecessaryColumns()
+    {
+
+    }
+
+    public function testConvertCanHandleEmptyResultSet()
+    {
+
+    }
+
+}
