de81388441b03dc45b268733240cd22468143b1c
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 17:04:32 +0100

    added Null object converter stub (#13)

---
+++ library/Erfurt/Store/Adapter/ResultConverter/NullConverter.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Null object implementation of a result set converter that does not change the provided data.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.12.13
+ */
+class Erfurt_Store_Adapter_ResultConverter_NullConverter
+{
+
+
+
+}
---
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/NullConverterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Tests the null object implementation of a result converter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.12.13
+ */
+class Erfurt_Store_Adapter_ResultConverter_NullConverterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Checks if the converter implements the required interface.
+     */
+    public function testImplementsInterface()
+    {
+
+    }
+
+    /**
+     * Ensures that convert() returns the passed input value.
+     */
+    public function testConvertReturnsInputValue()
+    {
+
+    }
+
+}
