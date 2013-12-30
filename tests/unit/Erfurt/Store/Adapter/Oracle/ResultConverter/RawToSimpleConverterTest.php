837a39705a0445f185beb1535e2310ff5675232c
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 14:44:36 +0100

    added comments (#13)

--- tests/unit/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToSimpleConverterTest.php
+++ tests/unit/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToSimpleConverterTest.php
@@ -9,26 +9,42 @@
 class Erfurt_Store_Adapter_Oracle_RawToSimpleConverterTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if the converter implements the necessary interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Ensures that an exception is thrown by convert() if no array is passed.
+     */
     public function testConvertThrowsExceptionIfNoArrayIsPassed()
     {
 
     }
 
+    /**
+     * Checks if variable names are converted into lower case.
+     */
     public function testConvertChangesVariablesToLowerCase()
     {
 
     }
 
+    /**
+     * Ensures that unnecessary columns (variable types, order values etc.)
+     * are removed from the result set.
+     */
     public function testConvertRemovesUnnecessaryColumns()
     {
 
     }
 
+    /**
+     * Ensures that convert() can handle an empty result set.
+     */
     public function testConvertCanHandleEmptyResultSet()
     {
 
