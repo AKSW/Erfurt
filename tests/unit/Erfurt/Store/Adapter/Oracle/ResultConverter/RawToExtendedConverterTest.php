8595659285ccceadda6da7cf663e328becba6360
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 15:22:41 +0100

    added test stub (#13)

---
+++ tests/unit/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToExtendedConverterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Tests the RawToExtended result set converter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.12.13
+ */
+class Erfurt_Store_Adapter_Oracle_ResultConverter_RawToExtendedConverterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterface()
+    {
+
+    }
+
+    public function testConvertThrowsExceptionIfNoArrayIsPassed()
+    {
+
+    }
+
+    public function testConvertAddsVariablesToHeadSection()
+    {
+
+    }
+
+    public function testConvertAddsCorrectNumberOfBindings()
+    {
+
+    }
+
+    public function testConvertAddsCorrectVariableTypesToBindings()
+    {
+
+    }
+
+    public function testConvertAssignsCorrectVariableValues()
+    {
+        
+    }
+
+    public function testConvertCanHandleEmptyArrays()
+    {
+
+    }
+
+}
