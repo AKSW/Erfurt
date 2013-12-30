e7f84b73b623cf9704bcfdd650d78d49a0bc4940
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 14:35:03 +0100

    added converter stub (#13)

---
+++ library/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToSimpleConverter.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Converts an raw Oracle result set into a simple form, which contains all
+ * variables in lower case and does not provide additional information such
+ * as the type of a variable.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.12.13
+ */
+class Erfurt_Store_Adapter_Oracle_RawToSimpleConverter
+    implements Erfurt_Store_Adapter_ResultConverter_ResultConverterInterface
+{
+
+    /**
+     * Converts the provided result set.
+     *
+     * @param array(array(string=>string))|mixed $resultSet
+     * @return array(array(string=>string))|mixed
+     */
+    public function convert($resultSet)
+    {
+        // TODO: Implement convert() method.
+    }
+
+}
