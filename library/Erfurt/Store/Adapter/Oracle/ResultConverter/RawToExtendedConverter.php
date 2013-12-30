d55bcea7441acf1023ddc0c8d756a58ef648bb76
Matthias Molitor <matthias@matthimatiker.de>
2013-12-30 15:22:27 +0100

    added converter stub (#13)

---
+++ library/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToExtendedConverter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Converts a raw Oracle result set into the extended format.
+ *
+ * The extended format contains additional information about
+ * the variable types.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.12.13
+ */
+class Erfurt_Store_Adapter_Oracle_ResultConverter_RawToExtendedConverter
+{
+
+}
