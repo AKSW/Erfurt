d3ff181961da1352ae6f86cf69abf5f557b4f420
Matthias Molitor <matthias@matthimatiker.de>
2013-12-31 00:46:56 +0100

    added result converter stub (#13)

---
+++ library/Erfurt/Store/Adapter/ResultConverter/ScalarConverter.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Extracts a single scalar value from a result set.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 31.12.13
+ */
+class Erfurt_Store_Adapter_ResultConverter_ScalarConverter
+    implements \Erfurt_Store_Adapter_ResultConverter_ResultConverterInterface
+{
+
+    /**
+     * Converts the provided result set.
+     *
+     * @param array(array(string=>string))|mixed $resultSet
+     * @return array(array(string=>string))|mixed
+     * @throws \Erfurt_Store_Adapter_ResultConverter_Exception If conversion is not possible.
+     */
+    public function convert($resultSet)
+    {
+        // TODO: Implement convert() method.
+    }
+
+}
