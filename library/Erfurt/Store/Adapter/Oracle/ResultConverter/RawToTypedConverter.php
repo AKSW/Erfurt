4c79c94471e152c3accd018061b937399b770774
Matthias Molitor <matthias@matthimatiker.de>
2013-12-31 11:59:14 +0100

    added converter stub (#13)

---
+++ library/Erfurt/Store/Adapter/Oracle/ResultConverter/RawToTypedConverter.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Accepts a raw Oracle result set and converts the values according to their
+ * type definition.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 31.12.13
+ */
+class Erfurt_Store_Adapter_Oracle_ResultConverter_RawToTypedConverter
+{
+
+}
