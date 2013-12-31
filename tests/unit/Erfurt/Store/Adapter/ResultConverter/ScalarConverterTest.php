978a19e02cdb5e7861f985a1ece82196e8c0fe70
Matthias Molitor <matthias@matthimatiker.de>
2013-12-31 11:22:54 +0100

    added test case stub (#13)

---
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/ScalarConverterTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Tests the Scalar result set converter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 31.12.13
+ */
+class Erfurt_Store_Adapter_ResultConverter_ScalarConverterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterface()
+    {
+
+    }
+
+    public function testConvertThrowsExceptionIfNoArrayIsPassed()
+    {
+
+    }
+
+    public function testConvertReturnsNullIfResultSetIsEmpty()
+    {
+
+    }
+
+    public function testConvertReturnsFirstValueInFirstRow()
+    {
+        
+    }
+
+}
