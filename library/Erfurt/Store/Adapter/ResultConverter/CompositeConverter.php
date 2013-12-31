9bcbc5e0fd9547724233970512cbb7093577c3d6
Matthias Molitor <matthias@matthimatiker.de>
2013-12-31 15:21:25 +0100

    added composite converter stub; added test case stub (#13)

---
+++ library/Erfurt/Store/Adapter/ResultConverter/CompositeConverter.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Contains several converters that are applied to a result set
+ * one after another.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 31.12.13
+ */
+class Erfurt_Store_Adapter_ResultConverter_CompositeConverter
+{
+
+}
---
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/CompositeConverterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Tests the composite converter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 31.12.13
+ */
+class Erfurt_Store_Adapter_ResultConverter_CompositeConverterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterface()
+    {
+
+    }
+
+    public function testCompositeReturnsUnchangedResultIfNoConverterIsProvided()
+    {
+
+    }
+
+    public function testCompositePassesResultSetToFirstConverter()
+    {
+
+    }
+
+    public function testCompositePassesResultFromFirstToSecondConverter()
+    {
+
+    }
+
+    public function testCompositeReturnsResultFromLastConverter()
+    {
+        
+    }
+
+}
