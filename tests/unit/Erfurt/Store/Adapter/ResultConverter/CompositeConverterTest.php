ae4ca50b17038efa74447e8d6264df80611b78c4
Matthias Molitor <matthias@matthimatiker.de>
2013-12-31 15:37:38 +0100

    added comments (#13)

--- tests/unit/Erfurt/Store/Adapter/ResultConverter/CompositeConverterTest.php
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/CompositeConverterTest.php
@@ -9,29 +9,46 @@
 class Erfurt_Store_Adapter_ResultConverter_CompositeConverterTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if the converter implements the necessary interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Ensures that  the unmodified result set is returned by convert() if no
+     * converter was passed to the composite.
+     */
     public function testCompositeReturnsUnchangedResultIfNoConverterIsProvided()
     {
 
     }
 
+    /**
+     * Checks if the result set is passed to the first converter.
+     */
     public function testCompositePassesResultSetToFirstConverter()
     {
 
     }
 
+    /**
+     * Ensures that the result of the first converter is passed to
+     * the second one.
+     */
     public function testCompositePassesResultFromFirstToSecondConverter()
     {
 
     }
 
+    /**
+     * Checks if the result of the last converter is returned by convert().
+     */
     public function testCompositeReturnsResultFromLastConverter()
     {
-        
+
     }
 
 }
