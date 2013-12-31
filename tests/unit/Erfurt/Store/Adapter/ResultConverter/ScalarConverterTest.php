bbd5d52518518427ced78544ef060162bd46731a
Matthias Molitor <matthias@matthimatiker.de>
2013-12-31 11:24:34 +0100

    added comments (#13)

--- tests/unit/Erfurt/Store/Adapter/ResultConverter/ScalarConverterTest.php
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/ScalarConverterTest.php
@@ -9,24 +9,36 @@
 class Erfurt_Store_Adapter_ResultConverter_ScalarConverterTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if the converter implements the required interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Ensures that convert() throws an exception if no array is passed.
+     */
     public function testConvertThrowsExceptionIfNoArrayIsPassed()
     {
 
     }
 
+    /**
+     * Ensures that convert() returns null if the provided result set is empty.
+     */
     public function testConvertReturnsNullIfResultSetIsEmpty()
     {
 
     }
 
+    /**
+     * Checks if convert() returns the first value in the first row of the result set.
+     */
     public function testConvertReturnsFirstValueInFirstRow()
     {
-        
+
     }
 
 }
