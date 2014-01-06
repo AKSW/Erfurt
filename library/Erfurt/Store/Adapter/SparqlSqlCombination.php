8d7c819f96c3c8d2b8ce5c65f37633d5b6a8b219
Matthias Molitor <matthias@matthimatiker.de>
2014-01-06 19:02:41 +0100

    added class stub for SPARQL/SQL combination (#13)

---
+++ library/Erfurt/Store/Adapter/SparqlSqlCombination.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class that combines a SPARQL as well as a SQL adapter and just delegates
+ * the calls to the interface methods.
+ *
+ * This allows one to separate SPARQL and SQL adapters and gives the opportunity
+ * to create any combination of these.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 06.01.14
+ */
+class Erfurt_Store_Adapter_SparqlSqlCombination
+{
+
+}
