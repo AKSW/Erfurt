5bcb3502d7b0f9c1c1070768f456fda2b8aa1bd2
Matthias Molitor <matthias@matthimatiker.de>
2014-01-06 19:04:51 +0100

    added notice

--- library/Erfurt/Store/Adapter/SparqlSqlCombination.php
+++ library/Erfurt/Store/Adapter/SparqlSqlCombination.php
@@ -6,6 +6,8 @@
  *
  * This allows one to separate SPARQL and SQL adapters and gives the opportunity
  * to create any combination of these.
+ * Without these separation each SPARQL adapter must implement the SQL interface
+ * to enable versioning support, even if SQL is not supported by it's architecture.
  *
  * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
  * @since 06.01.14
