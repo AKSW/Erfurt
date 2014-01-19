11af7c8b30614b310937e629735de21f79a64114
Matthias Molitor <matthias@matthimatiker.de>
2014-01-19 19:23:20 +0100

    added stub for JSON converter and test case (#13)

---
+++ library/Erfurt/Store/Adapter/ResultConverter/ToJsonConverter.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Converts a result set into a JSON string.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 19.01.14
+ */
+class Erfurt_Store_Adapter_ResultConverter_ToJsonConverter
+    implements \Erfurt_Store_Adapter_ResultConverter_ResultConverterInterface
+{
+
+    /**
+     * Converts the given result set into JSON.
+     *
+     * @param array(array(string=>string))|mixed $resultSet
+     * @return string
+     */
+    public function convert($resultSet)
+    {
+        
+    }
+
+}
