7ccca2a535d46052e3293e5b175ba8e7515d92f0
Matthias Molitor <matthias@matthimatiker.de>
2014-01-25 15:21:32 +0100

    added test case (#18)

---
+++ tests/unit/Erfurt/Store/Adapter/Oracle/QueryRewriterTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Tests the query rewriter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 25.01.14
+ */
+class Erfurt_Store_Adapter_Oracle_QueryRewriterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * System under test.
+     *
+     * @var Erfurt_Store_Adapter_Oracle_QueryRewriter
+     */
+    protected $rewriter = null;
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->rewriter = new Erfurt_Store_Adapter_Oracle_QueryRewriter();
+    }
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->rewriter = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Ensures that invalid queries are not modified by the rewriter.
+     */
+    public function testRewriterDoesNotModifyInvalidQueries()
+    {
+        $query = 'Hello world!';
+
+        $rewritten = $this->rewriter->rewrite($query);
+
+        $this->assertEquals($query, $rewritten);
+    }
+
+    /**
+     * Checks if query variables are prefixed by the rewriter.
+     */
+    public function testRewriterPrefixesVariableNames()
+    {
+        $query = 'SELECT ?subject FROM '
+               . '<http://example.org> '
+               . 'WHERE { ?subject ?predicate ?object . } '
+               . 'ORDER BY ASC(?object)';
+
+        $rewritten = $this->rewriter->rewrite($query);
+
+        // Checks if the query contains the prefixed variables...
+        $this->assertContains('?'. $this->prefix('subject'), $rewritten);
+        $this->assertContains('?'. $this->prefix('predicate'), $rewritten);
+        $this->assertContains('?'. $this->prefix('object'), $rewritten);
+        // ... and ensure that no old name is left.
+        $this->assertNotContains('?subject', $rewritten);
+        $this->assertNotContains('?predicate', $rewritten);
+        $this->assertNotContains('?object', $rewritten);
+    }
+
+    /**
+     * Checks if the rewriter encodes upper case characters.
+     */
+    public function testRewriterEncodesUpperCaseCharactersInVariables()
+    {
+        $query = 'SELECT ?camelCase WHERE { ?camelCase ?predicate ?object . }';
+
+        $rewritten = $this->rewriter->rewrite($query);
+
+        $this->assertContains('camel_case', $rewritten);
+    }
+
+    /**
+     * Prepends the prefix to the given variable name.
+     *
+     * @param string $variable
+     * @return string
+     */
+    protected function prefix($variable)
+    {
+        return Erfurt_Store_Adapter_Oracle_QueryRewriter::VARIABLE_PREFIX . $variable;
+    }
+
+}
