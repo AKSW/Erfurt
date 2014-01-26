41e4fb569f0b3cbb6617de7563040075c252e3bc
Matthias Molitor <matthias@matthimatiker.de>
2014-01-26 17:16:14 +0100

    fetch only associative entries (#13)

--- library/Erfurt/Store/Adapter/Oracle/OracleSqlAdapter.php
+++ library/Erfurt/Store/Adapter/Oracle/OracleSqlAdapter.php
@@ -161,7 +161,7 @@ class Erfurt_Store_Adapter_Oracle_OracleSqlAdapter implements Erfurt_Store_Sql_I
             }
             $statement = $this->connection->prepare($converted->query);
             $statement->execute($converted->params);
-            $rows = $statement->fetchAll();
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
             return array_map(function (array $row) {
                 return array_change_key_case($row, CASE_LOWER);
             }, $rows);
