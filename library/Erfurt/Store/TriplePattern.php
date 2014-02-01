daf197c1471469c4bd29065552f879c43ab24ff1
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 17:05:08 +0100

    moved members from Triple up to TriplePattern (#29)

--- library/Erfurt/Store/TriplePattern.php
+++ library/Erfurt/Store/TriplePattern.php
@@ -13,4 +13,118 @@
 class Erfurt_Store_TriplePattern
 {
 
+    /**
+     * The subject URI or blank node identifier.
+     *
+     * Null if any value is allowed.
+     *
+     * @var string|null
+     */
+    protected $subject = null;
+
+    /**
+     * The predicate URI.
+     *
+     * Null if any value is allowed.
+     *
+     * @var string|null
+     */
+    protected $predicate = null;
+
+    /**
+     * The object definition (URI, blank node or literal).
+     *
+     * Null if any value is allowed.
+     *
+     * @var array(string=>string)|null
+     */
+    protected $object = array();
+
+    /**
+     * Creates a triple that contains the given components.
+     *
+     * Null can be passed to indicate that every value is allowed
+     * at that position.
+     *
+     * @param string|null $subject
+     * @param string|null $predicate
+     * @param array(string=>string)|null $object
+     */
+    public function __construct($subject, $predicate, array $object)
+    {
+        $this->subject   = $subject;
+        $this->predicate = $predicate;
+        $this->object    = $object;
+    }
+
+    /**
+     * Returns the subject.
+     *
+     * @return string|null
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Returns the predicate.
+     *
+     * @return string|null
+     */
+    public function getPredicate()
+    {
+        return $this->predicate;
+    }
+
+    /**
+     * Returns the object definition.
+     *
+     * @return array(string=>string)|null
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * Returns the triple in a Turtle-like format.
+     *
+     * Example:
+     *
+     *     $triple = new Erfurt_Store_Triple(
+     *         'http://example.org/subject',
+     *         'http://example.org/predicate',
+     *         array('type' => 'uri', 'value' => 'http://example.org/object')
+     *     );
+     *     // Generates:
+     *     // <http://example.org/subject> <http://example.org/predicate> <http://example.org/object> .
+     *     $turtle = (string)$triple;
+     *
+     * Null placeholders will be represented as variable identifiers, for example "?subject".
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if ($this->object['type'] === 'uri') {
+            $object = '<' . $this->object['value'] . '>';
+        } else if ($this->object['type'] === 'bnode') {
+            $object = $this->object['value'];
+        } else {
+            $object = Erfurt_Utils::buildLiteralString(
+                $this->object['value'],
+                isset($this->object['datatype']) ? $this->object['datatype'] : null,
+                isset($this->object['lang']) ? $this->object['lang'] : null
+            );
+        }
+        if (strpos($this->subject, '_:') === 0) {
+            // Subject is a blank node.
+            $subject = $this->subject;
+        } else {
+            $subject = '<' . $this->subject . '>';
+        }
+        $template = '%s <%s> %s .';
+        return sprintf($template, $subject, $this->predicate, $object);
+    }
 }
---
+++ tests/unit/Erfurt/Store/TriplePatternTest.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * Tests the TriplePattern class.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 09.01.14
+ */
+class Erfurt_Store_TriplePatternTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * System under test.
+     *
+     * @var Erfurt_Store_TriplePattern
+     */
+    protected $triplePattern = null;
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->triplePattern = new Erfurt_Store_TriplePattern(
+            'http://example.org/subject',
+            'http://example.org/predicate',
+            array('type' => 'uri', 'value' => 'http://example.org/object')
+        );
+    }
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->triplePattern = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Checks if getSubject() returns the correct value.
+     */
+    public function testGetSubjectReturnsCorrectValue()
+    {
+        $this->assertEquals('http://example.org/subject', $this->triplePattern->getSubject());
+    }
+
+    /**
+     * Checks if getPredicate() returns the correct value.
+     */
+    public function testGetPredicateReturnsCorrectValue()
+    {
+        $this->assertEquals('http://example.org/predicate', $this->triplePattern->getPredicate());
+    }
+
+    /**
+     * Checks if getObject() returns the correct value.
+     */
+    public function testGetObjectReturnsCorrectValue()
+    {
+        $expectedObject = array('type' => 'uri', 'value' => 'http://example.org/object');
+        $this->assertEquals($expectedObject, $this->triplePattern->getObject());
+    }
+
+    /**
+     * Checks if __toString() formats a triple that contains only URIs correctly.
+     */
+    public function testToStringFormatsTripleWithUriObjectCorrectly()
+    {
+        $turtle = (string)$this->triplePattern;
+
+        $expected = '<http://example.org/subject> '
+                  . '<http://example.org/predicate> '
+                  . '<http://example.org/object> .';
+        $this->assertEquals($expected, $turtle);
+    }
+
+    /**
+     * Checks if __toString() formats a triple with literal object correctly.
+     */
+    public function testToStringFormatsTripleWithLiteralObjectCorrectly()
+    {
+        $triple = new Erfurt_Store_Triple(
+            'http://example.org/subject',
+            'http://example.org/predicate',
+            array('type' => 'literal', 'value' => 'Hello world!')
+        );
+        $turtle = (string)$triple;
+
+        $expected = '<http://example.org/subject> '
+                  . '<http://example.org/predicate> '
+                  . '"Hello world!" .';
+        $this->assertEquals($expected, $turtle);
+    }
+
+    /**
+     * Checks if __toString() formats blank nodes in the triple correctly.
+     */
+    public function testToStringFormatsBlankNodesCorrectly()
+    {
+        $triple = new Erfurt_Store_Triple(
+            '_:subjectNode',
+            'http://example.org/predicate',
+            array('type' => 'bnode', 'value' => '_:objectNode')
+        );
+        $turtle = (string)$triple;
+
+        $expected = '_:subjectNode '
+                  . '<http://example.org/predicate> '
+                  . '_:objectNode .';
+        $this->assertEquals($expected, $turtle);
+    }
+
+    /**
+     * Checks if the __toString() method replaces null values (which
+     * represent any value) by variables.
+     */
+    public function testToStringReplacesNullValuesByVariables()
+    {
+        $triple = new Erfurt_Store_Triple(
+            null,
+            null,
+            null
+        );
+        $turtle = (string)$triple;
+
+        $expected = '?subject '
+                  . '?predicate '
+                  . '?object .';
+        $this->assertEquals($expected, $turtle);
+    }
+
+}
