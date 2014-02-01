a3f41c8e98e3ef682271d232cd2ac62c1c065eac
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 18:06:43 +0100

    improved documentation of required method (#29)

--- library/Erfurt/Store/Adapter/Sparql/SparqlConnectorInterface.php
+++ library/Erfurt/Store/Adapter/Sparql/SparqlConnectorInterface.php
@@ -23,6 +23,40 @@ interface Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
     /**
      * Executes the provided SPARQL query and returns its results.
      *
+     * The results of an ASK query must be returned as boolean.
+     *
+     * If the query produces a result set, then it must be returned as array
+     * in extended format.
+     * The extended format each value contains additional information about
+     * its type and properties such as the language:
+     *
+     *     array(
+     *         'head' => array(
+     *             'vars' => array(
+     *                 // Contains the names of all variables that occur in the result set.
+     *                 'variable1',
+     *                 'variable2'
+     *             )
+     *         )
+     *         'results' => array(
+     *             'bindings' => array(
+     *                 // Contains one entry for each result set row.
+     *                 // Each entry contains the variable name as key and a set
+     *                 // of additional information as value:
+     *                 array(
+     *                     'variable1' => array(
+     *                         'value' => 'http://example.org',
+     *                         'type'  => 'uri'
+     *                     ),
+     *                     'variable2' => array(
+     *                         'value' => 'Hello world!',
+     *                         'type'  => 'literal'
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     *
      * @param string $sparqlQuery
      * @return array|boolean
      */
