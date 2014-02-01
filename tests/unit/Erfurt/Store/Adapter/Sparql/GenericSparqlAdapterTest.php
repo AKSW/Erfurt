09f54f542e6c60afaba877e4236fc6b0748a0d17
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 18:07:00 +0100

    added test case stub (#29)

---
+++ tests/unit/Erfurt/Store/Adapter/Sparql/GenericSparqlAdapterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Tests the generic SPARQL adapter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 01.02.14
+ */
+class Erfurt_Store_Adapter_Sparql_GenericSparqlAdapterTest extends \PHPUnit_Framework_TestCase
+{
+
+}
