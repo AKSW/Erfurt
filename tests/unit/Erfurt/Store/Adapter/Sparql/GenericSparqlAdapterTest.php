e3d0007abe560e360bd69e8a797b574a2c42ceaf
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 18:09:26 +0100

    added initialization code (#29)

--- tests/unit/Erfurt/Store/Adapter/Sparql/GenericSparqlAdapterTest.php
+++ tests/unit/Erfurt/Store/Adapter/Sparql/GenericSparqlAdapterTest.php
@@ -9,4 +9,38 @@
 class Erfurt_Store_Adapter_Sparql_GenericSparqlAdapterTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * System under test.
+     *
+     * @var \Erfurt_Store_Adapter_Sparql_GenericSparqlAdapter
+     */
+    protected $adapter = null;
+
+    /**
+     * The (mocked) connector that is used by the adapter.
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $connector = null;
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->connector = $this->getMock('\Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface');
+        $this->adapter   = new Erfurt_Store_Adapter_Sparql_GenericSparqlAdapter($this->connector);
+    }
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->adapter   = null;
+        $this->connector = null;
+        parent::tearDown();
+    }
+
 }
