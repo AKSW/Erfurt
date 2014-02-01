d7fda28ed195943d4ead082f78f9ba6440f7a848
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 16:56:00 +0100

    added triple pattern class stub (#29)

---
+++ library/Erfurt/Store/TriplePattern.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Represents a triple pattern.
+ *
+ * A triple pattern may contain concrete values for subject, predicate and object,
+ * but it also allows null values, which indicate that every value is allowed at
+ * that position.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 01.02.14
+ */
+class Erfurt_Store_TriplePattern
+{
+
+}
