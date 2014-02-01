2653366ab13c1288747192a264833960e18ec025
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 17:36:32 +0100

    added method signatures (#29)

--- library/Erfurt/Store/Adapter/Sparql/SparqlConnectorInterface.php
+++ library/Erfurt/Store/Adapter/Sparql/SparqlConnectorInterface.php
@@ -12,4 +12,29 @@
 interface Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
 {
 
+    /**
+     * Adds the provided triple to the data store.
+     *
+     * @param string $graphIri
+     * @param Erfurt_Store_Triple $triple
+     */
+    public function addTriple($graphIri, \Erfurt_Store_Triple $triple);
+
+    /**
+     * Executes the provided SPARQL query and returns its results.
+     *
+     * @param string $sparqlQuery
+     * @return array|boolean
+     */
+    public function query($sparqlQuery);
+
+    /**
+     * Deletes all triples in the given graph that match the provided pattern.
+     *
+     * @param string $graphIri
+     * @param Erfurt_Store_TriplePattern $pattern
+     * @return integer The number of deleted triples.
+     */
+    public function deleteMatchingTriples($graphIri, Erfurt_Store_TriplePattern $pattern);
+
 }
