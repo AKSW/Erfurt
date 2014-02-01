7979ab90e6a0ea12637f5f3a5e31f19d19c6ec64
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 15:13:23 +0100

    added interface stub (#29)

---
+++ library/Erfurt/Store/Adapter/Sparql/SparqlConnectorInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Minimal interface for connecting Erfurt to a triple store.
+ *
+ * Classes that implement this interface must be used with the GenericSparql adapter,
+ * which provides the more verbose interface that is expected by Erfurt.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 01.02.14
+ */
+interface Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
+{
+
+}
