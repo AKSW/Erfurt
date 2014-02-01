cb2e35194f8b9515ecdc7000679b1282543f0f34
Matthias Molitor <matthias@matthimatiker.de>
2014-02-01 15:16:23 +0100

    added class stub (#29)

---
+++ library/Erfurt/Store/Adapter/Sparql/GenericSparqlAdapter.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provides implementations for most of the SPARQL related methods
+ * that are used by Erfurt.
+ *
+ * Uses objects of type \Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
+ * to connect to a triple store.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 01.02.14
+ */
+class Erfurt_Store_Adapter_Sparql_GenericSparqlAdapter
+{
+
+}
