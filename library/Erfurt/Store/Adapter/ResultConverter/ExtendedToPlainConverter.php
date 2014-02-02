58a96b2ec6ea0d2fc4ab79906915bc665968ee59
Matthias Molitor <matthias@matthimatiker.de>
2014-02-02 14:20:56 +0100

    implemented result converter (#29)

--- library/Erfurt/Store/Adapter/ResultConverter/ExtendedToPlainConverter.php
+++ library/Erfurt/Store/Adapter/ResultConverter/ExtendedToPlainConverter.php
@@ -7,6 +7,29 @@
  * @since 02.02.14
  */
 class Erfurt_Store_Adapter_ResultConverter_ExtendedToPlainConverter
+    implements \Erfurt_Store_Adapter_ResultConverter_ResultConverterInterface
 {
+
+    /**
+     * Converts a result set from extended to plain format.
+     *
+     * @param array(mixed) $resultSet
+     * @return array(array(string=>string))
+     */
+    public function convert($resultSet)
+    {
+        $plain = array();
+        $row   = 0;
+        foreach ($resultSet['results']['bindings'] as $binding) {
+            /* @var $binding array(string=>array(string=>string)) */
+            foreach ($binding as $name => $definition) {
+                /* @var $name string */
+                /* @var $definition array(string=>mixed) */
+                $plain[$row][$name] = $definition['value'];
+            }
+            $row++;
+        }
+        return $plain;
+    }
 
 }
