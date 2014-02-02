af5aac134c8687a9c01c125751916d71c8bf1afb
Matthias Molitor <matthias@matthimatiker.de>
2014-02-02 15:25:51 +0100

    added Oracle connector stub (#29)

---
+++ library/Erfurt/Store/Adapter/Oracle/OracleSparqlConnector.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * Connector for the Oracle Triple Store (named Semantic and Graph option).
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 02.02.14
+ */
+class Erfurt_Store_Adapter_Oracle_OracleSparqlConnector
+    implements \Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
+{
+
+    /**
+     * Adds the provided triple to the data store.
+     *
+     * @param string $graphIri
+     * @param Erfurt_Store_Adapter_Sparql_Triple $triple
+     */
+    public function addTriple($graphIri, \Erfurt_Store_Adapter_Sparql_Triple $triple)
+    {
+        // TODO: Implement addTriple() method.
+    }
+
+    /**
+     * Executes the provided SPARQL query and returns its results.
+     *
+     * The results of an ASK query must be returned as boolean.
+     *
+     * If the query produces a result set, then it must be returned as array
+     * in extended format.
+     * The extended format each value contains additional information about
+     * its type and properties such as the language:
+     *
+     *     array(
+     *         'head' => array(
+     *             'vars' => array(
+     *                 // Contains the names of all variables that occur in the result set.
+     *                 'variable1',
+     *                 'variable2'
+     *             )
+     *         )
+     *         'results' => array(
+     *             'bindings' => array(
+     *                 // Contains one entry for each result set row.
+     *                 // Each entry contains the variable name as key and a set
+     *                 // of additional information as value:
+     *                 array(
+     *                     'variable1' => array(
+     *                         'value' => 'http://example.org',
+     *                         'type'  => 'uri'
+     *                     ),
+     *                     'variable2' => array(
+     *                         'value' => 'Hello world!',
+     *                         'type'  => 'literal'
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     *
+     * @param string $sparqlQuery
+     * @return array|boolean
+     */
+    public function query($sparqlQuery)
+    {
+        // TODO: Implement query() method.
+    }
+
+    /**
+     * Deletes all triples in the given graph that match the provided pattern.
+     *
+     * @param string $graphIri
+     * @param Erfurt_Store_Adapter_Sparql_TriplePattern $pattern
+     * @return integer The number of deleted triples.
+     */
+    public function deleteMatchingTriples($graphIri, Erfurt_Store_Adapter_Sparql_TriplePattern $pattern)
+    {
+        // TODO: Implement deleteMatchingTriples() method.
+    }
+
+    /**
+     * Accepts a callback function and processes it in batch mode.
+     *
+     * In batch mode the connector can decide to optimize the execution
+     * for example by delaying inserts or wrapping the whole task
+     * into a transaction.
+     *
+     * However, using the batch mode does *not* guarantee transactional
+     * behavior.
+     *
+     * The callback receives the connector itself as argument, which
+     * can be used to issue commands:
+     *
+     *     $connector->batch(function (\Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface $batchConnector) {
+     *         $batchConnector->addTriple(
+     *             'http://example.org',
+     *             new \Erfurt_Store_Adapter_Sparql_Triple(
+     *                 'http://example.org/subject1',
+     *                 'http://example.org/predicate1',
+     *                 'http://example.org/object1'
+     *             );
+     *         );
+     *         $batchConnector->addTriple(
+     *             'http://example.org',
+     *             new \Erfurt_Store_Adapter_Sparql_Triple(
+     *                 'http://example.org/subject2',
+     *                 'http://example.org/predicate2',
+     *                 'http://example.org/object2'
+     *             );
+     *         );
+     *     });
+     *
+     * Finally, the batch() method returns the result of the provided callback:
+     *
+     *     // Result contains 42.
+     *     $result = $connector->batch(function () {
+     *         return 42;
+     *     });
+     *
+     * @param mixed $callback A callback function.
+     * @return mixed
+     */
+    public function batch($callback)
+    {
+        // TODO: Implement batch() method.
+    }
+
+
+}
---
+++ tests/integration/Erfurt/Store/Adapter/Oracle/OracleSparqlConnectorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Tests the Oracle SPARQL connector.
+ *
+ * Erfurt_Store_Adapter_Oracle_
+ * @since 02.02.14
+ */
+class Erfurt_Store_Adapter_Oracle_OracleSparqlConnectorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
