59f38e9e79b78007e9ec63ac8b9451064e905b3d
Matthias Molitor <matthias@matthimatiker.de>
2014-02-02 16:31:11 +0100

    removed namespace prefix from comment (#29)

--- tests/integration/Erfurt/Store/Adapter/Oracle/OracleSparqlConnectorTest.php
+++ tests/integration/Erfurt/Store/Adapter/Oracle/OracleSparqlConnectorTest.php
@@ -3,7 +3,6 @@
 /**
  * Tests the Oracle SPARQL connector.
  *
- * Erfurt_Store_Adapter_Oracle_
  * @since 02.02.14
  */
 class Erfurt_Store_Adapter_Oracle_OracleSparqlConnectorTest extends \Erfurt_OracleTestCase
