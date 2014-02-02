508cf0062c58468bca97d69713f4b7e3ff258d5f
Matthias Molitor <matthias@matthimatiker.de>
2014-02-02 14:18:22 +0100

    added converter stub (#29)

---
+++ library/Erfurt/Store/Adapter/ResultConverter/ExtendedToPlainConverter.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Result set converter that converts from extended to plain format.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 02.02.14
+ */
+class Erfurt_Store_Adapter_ResultConverter_ExtendedToPlainConverter
+{
+
+}
