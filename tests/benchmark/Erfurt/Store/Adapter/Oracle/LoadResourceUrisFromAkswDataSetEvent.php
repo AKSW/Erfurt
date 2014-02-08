c0c74f54b3dda3e98d3ca4b86e7115c6a845d81f
Matthias Molitor <matthias@matthimatiker.de>
2014-02-08 23:29:38 +0100

    added benchmark for the REGEX filter query that is executed on the AKSW data set by OntoWiki (#30, #31)

---
+++ tests/benchmark/Erfurt/Store/Adapter/Oracle/LoadResourceUrisFromAkswDataSetEvent.php
@@ -0,0 +1,125 @@
+<?php
+
+use Faker\Generator;
+
+/**
+ * Checks the performance of the query  {@link https://github.com/Matthimatiker/Erfurt/issues/31}
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 08.02.14
+ */
+class Erfurt_Store_Adapter_Oracle_LoadResourceUrisFromAkswDataSetEvent
+    extends Erfurt_Store_Adapter_Oracle_AbstractConnectorAthleticEvent
+{
+
+    /**
+     * Loads the AKSW data set.
+     *
+     * @param \Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface $connector
+     * @param \Faker\Generator $faker
+     */
+    public function populateStore(\Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface $connector, Generator $faker)
+    {
+        $path = __DIR__ . '/_files/aksw.rdf';
+        $parser     = Erfurt_Syntax_RdfParser::rdfParserWithFormat('rdf');
+        $statements = $parser->parse($path, Erfurt_Syntax_RdfParser::LOCATOR_FILE);
+        $triples    = new Erfurt_Store_Adapter_Sparql_TripleIterator($statements);
+        foreach ($triples as $triple) {
+            /* @var $triple Erfurt_Store_Adapter_Sparql_Triple */
+            $this->connector->addTriple('http://localhost/OntoWiki/aksw', $triple);
+        }
+    }
+
+    /**
+     * Executes the query without modifications.
+     *
+     * @Iterations 1
+     */
+    public function executeQuery()
+    {
+        $query = 'SELECT DISTINCT ?resourceUri '
+               . 'FROM <http://localhost/OntoWiki/aksw> '
+               . 'WHERE { '
+               . '     { '
+               . '        ?sub <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?resourceUri '
+               . '    } UNION { '
+               . '        ?sub <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?resourceUri '
+               . '    } OPTIONAL { '
+               . '        ?resourceUri <http://ns.ontowiki.net/SysOnt/hidden> ?reg '
+               . '    } OPTIONAL { '
+               . '         ?resourceUri <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super '
+               . '    } '
+               . '    FILTER (!isBLANK(?resourceUri)) '
+               . '    FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/1999/02/22-rdf-syntax-ns#")) '
+               . '    FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/2000/01/rdf-schema#")) '
+               . '    FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/2002/07/owl#")) '
+               . '    FILTER (!BOUND(?reg)) '
+               . '    FILTER (REGEX(STR(?super), "^http://www.w3.org/2002/07/owl#") || !BOUND(?super)) '
+               . '} '
+               . 'LIMIT 11';
+        $this->connector->query($query);
+    }
+
+    /**
+     * Executes the query with multiple REGEX filters combined into one.
+     *
+     * @Iterations 1
+     */
+    public function testExecuteQueryWithCombinedRegularExpressions()
+    {
+        $query = 'SELECT DISTINCT ?resourceUri '
+            . 'FROM <http://localhost/OntoWiki/aksw> '
+            . 'WHERE { '
+            . '     { '
+            . '        ?sub <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?resourceUri '
+            . '    } UNION { '
+            . '        ?sub <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?resourceUri '
+            . '    } OPTIONAL { '
+            . '        ?resourceUri <http://ns.ontowiki.net/SysOnt/hidden> ?reg '
+            . '    } OPTIONAL { '
+            . '         ?resourceUri <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super '
+            . '    } '
+            . '    FILTER (!isBLANK(?resourceUri)) '
+            . '    FILTER (!REGEX(STR(?resourceUri), "(^http://www.w3.org/1999/02/22-rdf-syntax-ns#|^http://www.w3.org/2000/01/rdf-schema#|^http://www.w3.org/2002/07/owl#)")) '
+            . '    FILTER (!BOUND(?reg)) '
+            . '    FILTER (REGEX(STR(?super), "^http://www.w3.org/2002/07/owl#") || !BOUND(?super)) '
+            . '} '
+            . 'LIMIT 11';
+        $this->connector->query($query);
+    }
+
+    /**
+     * Executes the query with REGEX filters moved to each part of the UNION statement.
+     *
+     * @Iterations 1
+     */
+    public function testExecuteQueryWith()
+    {
+        $query = 'SELECT DISTINCT ?resourceUri '
+            . 'FROM <http://localhost/OntoWiki/aksw> '
+            . 'WHERE { '
+            . '     { '
+            . '        ?sub <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?resourceUri '
+            . '        FILTER (!isBLANK(?resourceUri)) '
+            . '        FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/1999/02/22-rdf-syntax-ns#")) '
+            . '        FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/2000/01/rdf-schema#")) '
+            . '        FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/2002/07/owl#")) '
+            . '    } UNION { '
+            . '        ?sub <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?resourceUri '
+            . '        FILTER (!isBLANK(?resourceUri)) '
+            . '        FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/1999/02/22-rdf-syntax-ns#")) '
+            . '        FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/2000/01/rdf-schema#")) '
+            . '        FILTER (!REGEX(STR(?resourceUri), "^http://www.w3.org/2002/07/owl#")) '
+            . '    } OPTIONAL { '
+            . '        ?resourceUri <http://ns.ontowiki.net/SysOnt/hidden> ?reg '
+            . '    } OPTIONAL { '
+            . '         ?resourceUri <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?super '
+            . '    } '
+            . '    FILTER (!BOUND(?reg)) '
+            . '    FILTER (REGEX(STR(?super), "^http://www.w3.org/2002/07/owl#") || !BOUND(?super)) '
+            . '} '
+            . 'LIMIT 11';
+        $this->connector->query($query);
+    }
+
+}
