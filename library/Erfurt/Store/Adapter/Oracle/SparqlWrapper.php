133f352c663ba2e924dbc1214ab2a87443f07054
Matthias Molitor <matthias@matthimatiker.de>
2014-02-09 16:24:29 +0100

    added constructor signature (#31)

--- library/Erfurt/Store/Adapter/Oracle/SparqlWrapper.php
+++ library/Erfurt/Store/Adapter/Oracle/SparqlWrapper.php
@@ -10,4 +10,30 @@
 class Erfurt_Store_Adapter_Oracle_SparqlWrapper
 {
 
+    /**
+     * Creates a wrapper that selects data from the provided model.
+     *
+     * Uses $valueQuoter to escape values in the generated SQL queries.
+     *
+     * @param string $modelName
+     * @param callable $valueQuoter
+     * @throws \InvalidArgumentException If no valid callback is passed.
+     */
+    public function __construct($modelName, $valueQuoter)
+    {
+
+    }
+
+    /**
+     * Returns the SQL query that is used to actually execute
+     * the provided SPARQL query.
+     *
+     * @param string $query The SPARQL query.
+     * @return string The SQL query.
+     */
+    public function wrap($query)
+    {
+
+    }
+
 }
