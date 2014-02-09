d50a1ffa844b19e6d5a0a2e18744caccd57d0093
Matthias Molitor <matthias@matthimatiker.de>
2014-02-09 16:31:11 +0100

    added comments (#31)

--- tests/unit/Erfurt/Store/Adapter/Oracle/SparqlWrapperTest.php
+++ tests/unit/Erfurt/Store/Adapter/Oracle/SparqlWrapperTest.php
@@ -47,37 +47,73 @@ class Erfurt_Store_Adapter_Oracle_SparqlWrapperTest extends \PHPUnit_Framework_T
         new Erfurt_Store_Adapter_Oracle_SparqlWrapper('test_model', array($this, 'missing'));
     }
 
+    /**
+     * Ensures that wrap() throws an exception if the SPARQL query contains the internal
+     * escape sequence.
+     *
+     * That's not a perfect solution, but at least it is unlikely.
+     */
     public function testWrapThrowsExceptionIfSparqlQueryContainsInternalEscapeSequence()
     {
 
     }
 
+    /**
+     * Checks if wrap() adds the name of the model to the SQL.
+     */
     public function testWrapAddsModelName()
     {
 
     }
 
+    /**
+     * Checks if wrap() adds the SPARQL query to the SQL.
+     */
     public function testWrapAddSparqlQuery()
     {
 
     }
 
+    /**
+     * Ensures that wrap() adds the parallelization query hint if the SPARQL query
+     * contains many filters.
+     */
     public function testWrapAddsParallelizationHintIfSparqlQueryContainsManyFilters()
     {
 
     }
 
+    /**
+     * Ensures that wrap() does not add the parallelization query hint if the SPARQL query
+     * does not use many FILTER expressions.
+     */
     public function testWrapDoesNotAddParallelizationHintIfQueryDoesNotContainManyFilters()
     {
 
     }
 
+    /**
+     * Ensures that wrap() adds ORDER BY if the SPARQL query requires an ordered result.
+     */
     public function testWrapAddsOrderByIfSparqlQueryRequiresSorting()
     {
 
     }
 
+    /**
+     * Ensures that wrap() does not add ORDER BY if the SPARQL query does not
+     * request an ordered result set.
+     */
     public function testWrapDoesNotAddOrderByIfSparqlQueryDoesNotRequireSorting()
+    {
+
+    }
+
+    /**
+     * Checks if the wrapper activates the strict default graph option, which means
+     * that the default graph contains only triples without graph part.
+     */
+    public function testWrapActivatesStrictDefaultGraphOption()
     {
 
     }
