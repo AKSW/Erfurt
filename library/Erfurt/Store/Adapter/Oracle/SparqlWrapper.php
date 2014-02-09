ee1b9d3c5e4bf081a987392f86f0ef2edd11953e
Matthias Molitor <matthias@matthimatiker.de>
2014-02-09 15:36:00 +0100

    added class stub for SPARQL -> SQL rewriter (#31)

---
+++ library/Erfurt/Store/Adapter/Oracle/SparqlWrapper.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Creates SQL that is used to retrieve the results of a SPARQL query
+ * from the Oracle database.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 09.02.14
+ */
+class Erfurt_Store_Adapter_Oracle_SparqlWrapper
+{
+
+}
