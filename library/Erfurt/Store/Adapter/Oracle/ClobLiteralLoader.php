ea125655a9caba8d827ffb5a2d3916b85d5c8c1d
Matthias Molitor <matthias@matthimatiker.de>
2014-02-10 15:32:22 +0100

    implemented CLOB loader (#26)

--- library/Erfurt/Store/Adapter/Oracle/ClobLiteralLoader.php
+++ library/Erfurt/Store/Adapter/Oracle/ClobLiteralLoader.php
@@ -12,13 +12,30 @@ class Erfurt_Store_Adapter_Oracle_ClobLiteralLoader
 {
 
     /**
+     * The connection that is used to retrieve the literal data.
+     *
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $connection = null;
+
+    /**
+     * The statement that is used to load the values.
+     *
+     * Contains null if the statement was not prepared yet.
+     * Use getLoadStatement() to get a statement object.
+     *
+     * @var \Doctrine\DBAL\Driver\Statement|null
+     */
+    protected $loadStatement = null;
+
+    /**
      * Creates a loader that uses the provided connection.
      *
      * @param Connection $connection
      */
     public function __construct(Connection $connection)
     {
-
+        $this->connection = $connection;
     }
 
     /**
@@ -29,7 +46,28 @@ class Erfurt_Store_Adapter_Oracle_ClobLiteralLoader
      */
     public function load($valueId)
     {
+        $statement = $this->getLoadStatement();
+        $statement->execute(array('valueId' => $valueId));
+        $value = $statement->fetchColumn();
+        return ($value === false) ? null : $value;
+    }
 
+    /**
+     * Returns the statement that is used to load the CLOB value.
+     *
+     * The parameter "valueId" is required.
+     *
+     * @return \Doctrine\DBAL\Driver\Statement
+     */
+    protected function getLoadStatement()
+    {
+        if ($this->loadStatement === null) {
+            $query = 'SELECT d.triple.GET_OBJECT() '
+                   . 'FROM erfurt_semantic_data d '
+                   . 'WHERE d.triple.RDF_O_ID = :valueId';
+            $this->loadStatement = $this->connection->prepare($query);
+        }
+        return $this->loadStatement;
     }
 
 }
