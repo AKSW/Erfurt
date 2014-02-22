933a08ea7dc2eee7e4e87676756e44f1492fc620
Matthias Molitor <matthias@matthimatiker.de>
2014-02-22 16:32:07 +0100

    added test case stub (#34)

---
+++ tests/unit/Erfurt/Store/Adapter/Container/ContainerFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Tests the DI container factory.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 22.02.14
+ */
+class Erfurt_Store_Adapter_Container_ContainerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testCreateReturnsContainer()
+    {
+
+    }
+
+    public function testFactoryCreatesCacheFiles()
+    {
+
+    }
+
+    public function testSecondCallToCreateReturnsContainer()
+    {
+
+    }
+
+    public function testFactoryUpdatesContainerWhenConfigFileChanges()
+    {
+
+    }
+
+}
