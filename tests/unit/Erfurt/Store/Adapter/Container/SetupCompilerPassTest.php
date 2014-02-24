04e6b55da8f6e4ede22173cc247208b5089a517b
Matthias Molitor <matthias@matthimatiker.de>
2014-02-24 13:22:04 +0100

    added comments (#16)

--- tests/unit/Erfurt/Store/Adapter/Container/SetupCompilerPassTest.php
+++ tests/unit/Erfurt/Store/Adapter/Container/SetupCompilerPassTest.php
@@ -9,24 +9,38 @@
 class Erfurt_Store_Adapter_Container_SetupCompilerPassTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if the compiler pass implements the required interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Ensures that the setup instances are not called if the "erfurt.container.auto_setup"
+     * parameter is false.
+     */
     public function testSetupsAreNotCalledIfAutoSetupIsFalse()
     {
 
     }
 
+    /**
+     * Ensures that the setups are called if the "erfurt.container.auto_setup" evaluates
+     * to true.
+     */
     public function testPassCallsSetupsIfAutoSetupIsTrue()
     {
 
     }
 
+    /**
+     * Ensures that features that are already installed won't be installed again.
+     */
     public function testPassDoesNotReInstallFeatures()
     {
-        
+
     }
 
 }
