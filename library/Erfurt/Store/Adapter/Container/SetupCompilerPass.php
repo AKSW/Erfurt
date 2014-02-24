528b071817d565fdbf12ea6a52df884ba79a7750
Matthias Molitor <matthias@matthimatiker.de>
2014-02-24 14:05:08 +0100

    implemented compiler pass (#16)

--- library/Erfurt/Store/Adapter/Container/SetupCompilerPass.php
+++ library/Erfurt/Store/Adapter/Container/SetupCompilerPass.php
@@ -22,10 +22,37 @@ class Erfurt_Store_Adapter_Container_SetupCompilerPass implements CompilerPassIn
      * Executes the setup procedures if necessary.
      *
      * @param ContainerBuilder $container
+     * @throws \RuntimeException If an invalid service is tagged as setup.
      */
     public function process(ContainerBuilder $container)
     {
-
+        if (!$container->hasParameter('erfurt.container.auto_setup')) {
+            return;
+        }
+        if (!$container->getParameter('erfurt.container.auto_setup')) {
+            return;
+        }
+        // Auto setup is active, find Setup instances and perform an installation if necessary.
+        $setupIds = array_keys($container->findTaggedServiceIds('erfurt.container.setup'));
+        foreach ($setupIds as $id) {
+            /* @var $id string */
+            /* @var $setup \Erfurt_Store_Adapter_Container_SetupInterface */
+            $setup = $container->get($id);
+            if (!($setup instanceof Erfurt_Store_Adapter_Container_SetupInterface)) {
+                $message = 'Each service that is tagged as "%s" must implement the interface %s. '
+                         . 'Service %s does not fulfill this requirement';
+                $message = sprintf(
+                    $message,
+                    'erfurt.container.setup',
+                    'Erfurt_Store_Adapter_Container_SetupInterface',
+                    $id
+                );
+                throw new RuntimeException($message);
+            }
+            if (!$setup->isInstalled()) {
+                $setup->install();
+            }
+        }
     }
 
 }
