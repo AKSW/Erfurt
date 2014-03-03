2e785a25789bdfcfb6f30b5760b0c1985fab7def
Matthias Molitor <matthias@matthimatiker.de>
2014-03-03 17:25:49 +0100

    added first benchmark for Stardog (#28)

---
+++ tests/benchmark/Erfurt/Store/Adapter/Stardog/LoadSmallRowsEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Checks the performance when loading many (small) rows via Oracle SPARQL connector.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 03.03.14
+ */
+class Erfurt_Store_Adapter_Stardog_LoadSmallRowsEvent
+    extends Erfurt_Store_Adapter_Sparql_AbstractLoadSmallRowsAthleticEvent
+{
+
+}
