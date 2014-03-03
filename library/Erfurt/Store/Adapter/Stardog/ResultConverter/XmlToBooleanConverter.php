0e93c0ef21b9e4a781cef03af2ef44fbcd960650
Matthias Molitor <matthias@matthimatiker.de>
2014-03-03 14:37:05 +0100

    added converter stub (#28)

---
+++ library/Erfurt/Store/Adapter/Stardog/ResultConverter/XmlToBooleanConverter.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Converter that accepts a SimpleXml object and converts it to a boolean.
+ *
+ * The XML should have the structure of an ASK query result:
+ *
+ *     <?xml version='1.0' encoding='UTF-8'?>
+ *     <sparql xmlns='http://www.w3.org/2005/sparql-results#'>
+ *         <head>
+ *         </head>
+ *         <boolean>true</boolean>
+ *     </sparql>
+ *
+ * Other XML structures will always be converted to false.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 03.03.14
+ */
+class Erfurt_Store_Adapter_Stardog_ResultConverter_XmlToBooleanConverter
+{
+
+}
