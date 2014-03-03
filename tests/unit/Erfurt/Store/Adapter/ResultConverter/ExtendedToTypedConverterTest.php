3341247d7f3e30637bb6e99a7b2c57b52cad9c79
Matthias Molitor <matthias@matthimatiker.de>
2014-03-03 13:35:27 +0100

    added test case stub (#28)

---
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/ExtendedToTypedConverterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Tests the converter that casts literal values in an extended result set.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 03.03.14
+ */
+class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Checks if the converter implements the required interface.
+     */
+    public function testImplementsInterface()
+    {
+
+    }
+
+    /**
+     * Ensures that URI values are not changed.
+     */
+    public function testConverterDoesNotChangeUris()
+    {
+
+    }
+
+    /**
+     * Ensures that string literals are not changed.
+     */
+    public function testConverterDoesNotChangeString()
+    {
+
+    }
+
+    /**
+     * Checks if boolean literals are casted correctly.
+     */
+    public function testConverterCastsBooleanLiteralsCorrectly()
+    {
+
+    }
+
+    /**
+     * Checks if integer literals are casted correctly.
+     */
+    public function testConverterCastsIntegerValuesCorrectly()
+    {
+
+    }
+
+    /**
+     * Checks if double literals are casted correctly.
+     */
+    public function testConverterCastsDoubleValuesCorrectly()
+    {
+
+    }
+
+}
