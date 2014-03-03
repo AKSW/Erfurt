48a94f3d158bbc85e97f03447b51b6ac1d1f8f53
Matthias Molitor <matthias@matthimatiker.de>
2014-03-03 13:43:42 +0100

    implemented converter tests (#28)

--- tests/unit/Erfurt/Store/Adapter/ResultConverter/ExtendedToTypedConverterTest.php
+++ tests/unit/Erfurt/Store/Adapter/ResultConverter/ExtendedToTypedConverterTest.php
@@ -10,11 +10,36 @@ class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends 
 {
 
     /**
+     * System under test.
+     *
+     * @var \Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverter
+     */
+    protected $converter = null;
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->converter = new Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverter();
+    }
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->converter = null;
+        parent::tearDown();
+    }
+
+    /**
      * Checks if the converter implements the required interface.
      */
     public function testImplementsInterface()
     {
-
+        $this->assertInstanceOf('\Erfurt_Store_Adapter_ResultConverter_ResultConverterInterface', $this->converter);
     }
 
     /**
@@ -22,7 +47,16 @@ class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends 
      */
     public function testConverterDoesNotChangeUris()
     {
+        $value = array(
+            'type'  => 'uri',
+            'value' => 'http://example.org'
+        );
+        $result = $this->getExtendedResult($value);
 
+        $converted = $this->converter->convert($result);
+
+        $this->assertInternalType('array', $converted);
+        $this->assertEquals($value, $converted['results']['bindings'][0]['value']);
     }
 
     /**
@@ -30,7 +64,17 @@ class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends 
      */
     public function testConverterDoesNotChangeString()
     {
+        $value = array(
+            'type'     => 'literal',
+            'datatype' => 'http://www.w3.org/2001/XMLSchema#string',
+            'value'    => 'Hello world.'
+        );
+        $result = $this->getExtendedResult($value);
 
+        $converted = $this->converter->convert($result);
+
+        $this->assertInternalType('array', $converted);
+        $this->assertEquals($value, $converted['results']['bindings'][0]['value']);
     }
 
     /**
@@ -38,7 +82,18 @@ class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends 
      */
     public function testConverterCastsBooleanLiteralsCorrectly()
     {
+        $value = array(
+            'type'     => 'literal',
+            'datatype' => 'http://www.w3.org/2001/XMLSchema#boolean',
+            'value'    => 'false'
+        );
+        $result = $this->getExtendedResult($value);
 
+        $converted = $this->converter->convert($result);
+
+        $this->assertInternalType('array', $converted);
+        $convertedValue = $converted['results']['bindings'][0]['value'];
+        $this->assertFalse($convertedValue['value']);
     }
 
     /**
@@ -46,7 +101,19 @@ class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends 
      */
     public function testConverterCastsIntegerValuesCorrectly()
     {
+        $value = array(
+            'type'     => 'literal',
+            'datatype' => 'http://www.w3.org/2001/XMLSchema#integer',
+            'value'    => '42'
+        );
+        $result = $this->getExtendedResult($value);
 
+        $converted = $this->converter->convert($result);
+
+        $this->assertInternalType('array', $converted);
+        $convertedValue = $converted['results']['bindings'][0]['value'];
+        $this->assertInternalType('integer', $convertedValue['value']);
+        $this->assertEquals(42, $convertedValue['value']);
     }
 
     /**
@@ -54,7 +121,44 @@ class Erfurt_Store_Adapter_ResultConverter_ExtendedToTypedConverterTest extends 
      */
     public function testConverterCastsDoubleValuesCorrectly()
     {
+        $value = array(
+            'type'     => 'literal',
+            'datatype' => 'http://www.w3.org/2001/XMLSchema#double',
+            'value'    => '42.42'
+        );
+        $result = $this->getExtendedResult($value);
 
+        $converted = $this->converter->convert($result);
+
+        $this->assertInternalType('array', $converted);
+        $convertedValue = $converted['results']['bindings'][0]['value'];
+        $this->assertInternalType('float', $convertedValue['value']);
+        $this->assertEquals(42.42, $convertedValue['value']);
+    }
+
+    /**
+     * Creates a SPARQL result in extended that contains a single row with
+     * a single value (Variable: value) that is specified by $valueSpecification.
+     *
+     * @param array(string=>string)
+     * @return array(mixed)
+     */
+    protected function getExtendedResult(array $valueSpecification)
+    {
+        return array(
+            'head' => array(
+                'vars' => array(
+                    'value'
+                )
+            ),
+            'results' => array(
+                'bindings' => array(
+                    array(
+                        'value' => $valueSpecification
+                    )
+                )
+            )
+        );
     }
 
 }
