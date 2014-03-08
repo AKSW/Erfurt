9f14c06dfd018a63296ef8e26f4079f37d886364
Matthias Molitor <matthias@matthimatiker.de>
2014-03-08 13:08:45 +0100

    added test case stub (#28)

---
+++ tests/unit/Erfurt/Store/Adapter/Stardog/DataAccessClientTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Tests the data access client.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 08.03.14
+ */
+class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testQueryExecutesSparqlQuery()
+    {
+
+    }
+
+    public function testTransactionalThrowsExceptionIfNoValidCallbackIsPassed()
+    {
+
+    }
+
+    public function testTransactionalStartsAndCommitsTransaction()
+    {
+
+    }
+
+    public function testTransactionPerformsRollbackIfExceptionIsThrown()
+    {
+
+    }
+
+    public function testTransactionalPassesClientAsFirstArgumentToCallback()
+    {
+
+    }
+
+    public function testTransactionalExecutesQueriesWithInTransaction()
+    {
+
+    }
+
+    public function testTransactionalDoesNotStartMoreTransactionIfCalledAgainFromCallback()
+    {
+
+    }
+
+}
