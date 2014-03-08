06882194338371b3b2e50e7ae0053eb4307b77e5
Matthias Molitor <matthias@matthimatiker.de>
2014-03-08 13:58:50 +0100

    implemented data access client (#28)

--- library/Erfurt/Store/Adapter/Stardog/DataAccessClient.php
+++ library/Erfurt/Store/Adapter/Stardog/DataAccessClient.php
@@ -11,13 +11,27 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClient
 {
 
     /**
+     * The low-level client that is used to interact with the triple store.
+     *
+     * @var Erfurt_Store_Adapter_Stardog_ApiClient
+     */
+    protected $apiClient = null;
+
+    /**
+     * ID of the running transaction or null if no transaction is active.
+     *
+     * @var string|null
+     */
+    protected $runningTransaction = null;
+
+    /**
      * Creates a data access client that uses the provided API client.
      *
      * @param Erfurt_Store_Adapter_Stardog_ApiClient $client
      */
     public function __construct(Erfurt_Store_Adapter_Stardog_ApiClient $client)
     {
-
+        $this->apiClient = $client;
     }
 
     /**
@@ -32,7 +46,10 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClient
      */
     public function query($sparqlQuery)
     {
-
+        $arguments = array(
+            'query' => $sparqlQuery
+        );
+        return $this->apiClient->query($this->withTransaction($arguments));
     }
 
     /**
@@ -47,10 +64,81 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClient
      *
      * @param callback $callback
      * @throws InvalidArgumentException if no valid callback is passed.
+     * @throws Exception Any error from the executed callback.
      */
     public function transactional($callback)
     {
+        if (!is_callable($callback)) {
+            $message = __FUNCTION__ . '() expects a valid callback.';
+            throw new \InvalidArgumentException($message);
+        }
+        if ($this->inTransaction()) {
+            // A transaction is already active, therefore, this execution of transactional()
+            // is not responsible for transaction management.
+            call_user_func($callback, $this);
+            return;
+        }
+        $this->beginTransaction();
+        try {
+            call_user_func($callback, $this);
+            $this->commit();
+        } catch (Exception $e) {
+            $this->rollback();
+            throw $e;
+        }
+    }
 
+    /**
+     * Adds the current transaction ID to the given operation arguments.
+     *
+     * Does not change the arguments if no transaction is running.
+     *
+     * @param array(string=>string) $arguments
+     * @return array(string=>string)
+     */
+    protected function withTransaction(array $arguments)
+    {
+        if (!$this->inTransaction()) {
+            return $arguments;
+        }
+        $arguments['transaction-id'] = $this->runningTransaction;
+        return $arguments;
+    }
+
+    /**
+     * Starts a new transaction.
+     */
+    protected function beginTransaction()
+    {
+        $this->runningTransaction = $this->apiClient->beginTransaction();
+    }
+
+    /**
+     * Commits the running transaction.
+     */
+    protected function commit()
+    {
+        $this->apiClient->commitTransaction($this->withTransaction(array()));
+        $this->runningTransaction = null;
+    }
+
+    /**
+     * Performs a running for the current transaction.
+     */
+    protected function rollback()
+    {
+        $this->apiClient->rollbackTransaction($this->withTransaction(array()));
+        $this->runningTransaction = null;
+    }
+
+    /**
+     * Checks if the client is currently in a transaction.
+     *
+     * @return boolean
+     */
+    protected function inTransaction()
+    {
+        return $this->runningTransaction !== null;
     }
 
 }
