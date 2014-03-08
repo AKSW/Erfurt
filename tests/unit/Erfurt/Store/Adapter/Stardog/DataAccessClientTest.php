fc8d52cec67317deb5a1635273ec6d65bb8cbc4b
Matthias Molitor <matthias@matthimatiker.de>
2014-03-08 13:39:45 +0100

    implemented tests (#28)

--- tests/unit/Erfurt/Store/Adapter/Stardog/DataAccessClientTest.php
+++ tests/unit/Erfurt/Store/Adapter/Stardog/DataAccessClientTest.php
@@ -10,12 +10,53 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
 {
 
     /**
+     * System under test.
+     *
+     * @var Erfurt_Store_Adapter_Stardog_DataAccessClient
+     */
+    protected $client = null;
+
+    /**
+     * The mocked API client.
+     *
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $apiClient = null;
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->apiClient = $this->getMockBuilder('Erfurt_Store_Adapter_Stardog_ApiClient')
+                                ->disableOriginalConstructor()
+                                ->getMock();
+        $this->client = new Erfurt_Store_Adapter_Stardog_DataAccessClient($this->apiClient);
+    }
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->client    = null;
+        $this->apiClient = null;
+        parent::tearDown();
+    }
+
+    /**
      * Checks if query() executes the provided SPARQL query (which means that it is passed
      * to the API client).
      */
     public function testQueryExecutesSparqlQuery()
     {
+        $query = 'SELECT * WHERE { ?s ?p ?o . }';
+        $this->apiClient->expects($this->once())
+                        ->method('query')
+                        ->with(array('query' => $query));
 
+        $this->client->query($query);
     }
 
     /**
@@ -23,7 +64,14 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
      */
     public function testQueryReturnsResultFromApiClient()
     {
+        $simulatedResult = array('results' => array('bindings' => array()));
+        $this->apiClient->expects($this->once())
+                        ->method('query')
+                        ->will($this->returnValue($simulatedResult));
 
+        $result = $this->client->query('SELECT * WHERE { ?s ?p ?o . }');
+
+        $this->assertEquals($simulatedResult, $result);
     }
 
     /**
@@ -31,7 +79,8 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
      */
     public function testTransactionalThrowsExceptionIfNoValidCallbackIsPassed()
     {
-
+        $this->setExpectedException('InvalidArgumentException');
+        $this->client->transactional(new \stdClass());
     }
 
     /**
@@ -39,7 +88,14 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
      */
     public function testTransactionalStartsAndCommitsTransaction()
     {
+        $this->apiClient->expects($this->once())
+                        ->method('beginTransaction')
+                        ->will($this->returnValue('t42'));
+        $this->apiClient->expects($this->once())
+                        ->method('commitTransaction')
+                        ->with(array('transaction-id' => 't42'));
 
+        $this->client->transactional(function() {});
     }
 
     /**
@@ -48,7 +104,16 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
      */
     public function testTransactionPerformsRollbackIfExceptionIsThrown()
     {
+        $this->apiClient->expects($this->once())
+                        ->method('beginTransaction')
+                        ->will($this->returnValue('t42'));
+        $this->apiClient->expects($this->once())
+                        ->method('rollbackTransaction')
+                        ->with(array('transaction-id' => 't42'));
 
+        $this->client->transactional(function() {
+            throw new RuntimeException('Error within transaction.');
+        });
     }
 
     /**
@@ -57,7 +122,15 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
      */
     public function testTransactionalPassesClientAsFirstArgumentToCallback()
     {
+        $callback = $this->getMock('stdClass', array('__invoke'));
+        $callback->expects($this->once())
+                 ->method('__invoke')
+                 ->with($this->isInstanceOf(get_class($this->client)));
+        $this->apiClient->expects($this->any())
+                        ->method('beginTransaction')
+                        ->will($this->returnValue('t42'));
 
+        $this->client->transactional($callback);
     }
 
     /**
@@ -65,16 +138,44 @@ class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framewo
      */
     public function testTransactionalExecutesQueriesWithInTransaction()
     {
+        $constraint = $this->logicalAnd(
+            $this->isType('array'),
+            $this->arrayHasKey('transaction-id'),
+            $this->contains('t42')
+        );
+        $this->apiClient->expects($this->once())
+                        ->method('query')
+                        ->with($constraint);
+        $this->apiClient->expects($this->any())
+                        ->method('beginTransaction')
+                        ->will($this->returnValue('t42'));
 
+        $this->client->transactional(function ($client) {
+            /* @var $client Erfurt_Store_Adapter_Stardog_DataAccessClient */
+            PHPUnit_Framework_Assert::assertInstanceOf('Erfurt_Store_Adapter_Stardog_DataAccessClient', $client);
+            $client->query('SELECT * WHERE { ?s ?p ?o . }');
+        });
     }
 
     /**
      * Ensures that transactional() does not start another transaction if the provided callback
      * invokes transactional() again.
      */
-    public function testTransactionalDoesNotStartMoreTransactionIfCalledAgainFromCallback()
+    public function testTransactionalDoesNotStartMoreTransactionsIfCalledAgainFromCallback()
     {
+        $this->apiClient->expects($this->once())
+                        ->method('beginTransaction')
+                        ->will($this->returnValue('t42'));
+        $this->apiClient->expects($this->once())
+                        ->method('commitTransaction')
+                        ->with(array('transaction-id' => 't42'));
 
+        $this->client->transactional(function ($client) {
+            /* @var $client Erfurt_Store_Adapter_Stardog_DataAccessClient */
+            PHPUnit_Framework_Assert::assertInstanceOf('Erfurt_Store_Adapter_Stardog_DataAccessClient', $client);
+            $client->transactional(function() {});
+            $client->transactional(function() {});
+        });
     }
 
 }
