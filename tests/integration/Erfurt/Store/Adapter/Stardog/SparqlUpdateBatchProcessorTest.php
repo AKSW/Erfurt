5c85afba6c495a72fa230e73da6c0d20b018b230
Matthias Molitor <matthias@matthimatiker.de>
2014-03-08 15:05:28 +0100

    added comments (#28)

--- tests/integration/Erfurt/Store/Adapter/Stardog/SparqlUpdateBatchProcessorTest.php
+++ tests/integration/Erfurt/Store/Adapter/Stardog/SparqlUpdateBatchProcessorTest.php
@@ -36,26 +36,41 @@ class Erfurt_Store_Adapter_Stardog_SparqlUpdateBatchProcessorTest extends \PHPUn
         parent::tearDown();
     }
 
+    /**
+     * Checks if the batch processor implements the required interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Checks if persist() can handle an empty quad list.
+     */
     public function testPersistCanHandleEmptyQuadList()
     {
 
     }
 
+    /**
+     * Checks if persist() adds a singe quad.
+     */
     public function testPersistAddsSingleQuad()
     {
 
     }
 
+    /**
+     * Checks if persist() adds multiple quads.
+     */
     public function testPersistAddsMultipleQuads()
     {
 
     }
 
+    /**
+     * Ensures that persist() adds the triples to the correct graphs.
+     */
     public function testPersistAssignsQuadsToCorrectGraphs()
     {
 
