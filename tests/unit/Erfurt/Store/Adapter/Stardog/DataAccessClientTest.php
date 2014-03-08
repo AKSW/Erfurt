dff5cc787dfd9f7277f789560c247a09f086e5d4
Matthias Molitor <matthias@matthimatiker.de>
2014-03-08 13:17:41 +0100

    added comments (#28)

--- tests/unit/Erfurt/Store/Adapter/Stardog/DataAccessClientTest.php
+++ tests/unit/Erfurt/Store/Adapter/Stardog/DataAccessClientTest.php
@@ -9,41 +9,69 @@
 class Erfurt_Store_Adapter_Stardog_DataAccessClientTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if query() executes the provided SPARQL query (which means that it is passed
+     * to the API client).
+     */
     public function testQueryExecutesSparqlQuery()
     {
 
     }
 
+    /**
+     * Checks if query() returns the result from the API client.
+     */
     public function testQueryReturnsResultFromApiClient()
     {
-        
+
     }
 
+    /**
+     * Ensures that transactional() throws an exception if no valid callback is provided.
+     */
     public function testTransactionalThrowsExceptionIfNoValidCallbackIsPassed()
     {
 
     }
 
+    /**
+     * Ensures that transactional() starts and commits a transaction.
+     */
     public function testTransactionalStartsAndCommitsTransaction()
     {
 
     }
 
+    /**
+     * Ensures that transactional() performs a rollback if an exception
+     * is thrown by the callback.
+     */
     public function testTransactionPerformsRollbackIfExceptionIsThrown()
     {
 
     }
 
+    /**
+     * Checks if transactional() passes the data access client as first argument
+     * to the callback.
+     */
     public function testTransactionalPassesClientAsFirstArgumentToCallback()
     {
 
     }
 
+    /**
+     * Checks if transactional() executes queries within a transaction.
+     */
     public function testTransactionalExecutesQueriesWithInTransaction()
     {
 
     }
 
+    /**
+     * Ensures that transactional() does not start another transaction if the provided callback
+     * invokes transactional() again.
+     */
     public function testTransactionalDoesNotStartMoreTransactionIfCalledAgainFromCallback()
     {
 
