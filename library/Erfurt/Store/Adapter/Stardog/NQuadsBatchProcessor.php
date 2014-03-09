e35173bea21664c3585fd875e4d59d9a09f79864
Matthias Molitor <matthias@matthimatiker.de>
2014-03-09 14:59:41 +0100

    implemented NQuad batch processor (#28)

--- library/Erfurt/Store/Adapter/Stardog/NQuadsBatchProcessor.php
+++ library/Erfurt/Store/Adapter/Stardog/NQuadsBatchProcessor.php
@@ -10,13 +10,49 @@ class Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessor implements Erfurt_Store_
 {
 
     /**
+     * The client that is used to interact with the store.
+     *
+     * @var Erfurt_Store_Adapter_Stardog_DataAccessClient
+     */
+    protected $client = null;
+
+    /**
+     * Creates a batch processor that uses the provided client.
+     *
+     * @param Erfurt_Store_Adapter_Stardog_DataAccessClient $client
+     */
+    public function __construct(Erfurt_Store_Adapter_Stardog_DataAccessClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
      * Stores the provided quads.
      *
      * @param array(\Erfurt_Store_Adapter_Sparql_Quad) $quads
      */
     public function persist(array $quads)
     {
-        // TODO: Implement persist() method.
+        if (count($quads) === 0) {
+            return;
+        }
+        $this->client->import($this->toNQuads($quads), Erfurt_Store_Adapter_Stardog_DataAccessClient::FORMAT_NQUADS);
+    }
+
+    /**
+     * Converts the provided quads into NQuad format (subject, predicate, object and context).
+     *
+     * @param array(\Erfurt_Store_Adapter_Sparql_Quad) $quads
+     * @return string
+     */
+    protected function toNQuads(array $quads)
+    {
+        $data = '';
+        foreach ($quads as $quad) {
+            /* @var $quad \Erfurt_Store_Adapter_Sparql_Quad */
+            $data .= $quad->format('?subject ?predicate ?object ?graph .') . PHP_EOL;
+        }
+        return rtrim($data);
     }
 
 }
