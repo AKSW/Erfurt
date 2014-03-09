47322cb1bf665a2d53ff300c7d6648dc88209a5c
Matthias Molitor <matthias@matthimatiker.de>
2014-03-09 19:39:14 +0100

    implemented tests (#28)

--- tests/unit/Erfurt/Store/Adapter/Sparql/SizeDependentBatchProcessorTest.php
+++ tests/unit/Erfurt/Store/Adapter/Sparql/SizeDependentBatchProcessorTest.php
@@ -61,32 +61,88 @@ class Erfurt_Store_Adapter_Sparql_SizeDependentBatchProcessorTest extends \PHPUn
      */
     public function testImplementsInterface()
     {
-
+        $this->assertInstanceOf('Erfurt_Store_Adapter_Sparql_BatchProcessorInterface', $this->processor);
     }
 
+    /**
+     * Ensures that the processor does not pass empty quad lists to any sub-processor.
+     */
     public function testProcessorDoesNotPassEmptySets()
     {
+        $this->smallProcessor->expects($this->never())
+                             ->method('persist');
+        $this->hugeProcessor->expects($this->never())
+                            ->method('persist');
 
+        $this->processor->persist(array());
     }
 
+    /**
+     * Checks if the processor passes quad sets that are smaller than the provided
+     * size to the sub-processor that is responsible for small sets.
+     */
     public function testProcessorPassesSetsSmallerThanSizeToSmallProcessor()
     {
+        $this->smallProcessor->expects($this->once())
+                             ->method('persist')
+                             ->with(new PHPUnit_Framework_Constraint_Count(2));
+        $this->hugeProcessor->expects($this->never())
+                            ->method('persist');
 
+        $this->processor->persist($this->createQuadSet(2));
     }
 
-    public function testProcessorPassesSetsGreaterThanSizeToSmallProcessor()
+    /**
+     * Checks if the processor passes quad sets that are greater than the provided
+     * size to the sub-processor that is responsible for huge sets.
+     */
+    public function testProcessorPassesSetsGreaterThanSizeToHugeProcessor()
     {
+        $this->smallProcessor->expects($this->never())
+                             ->method('persist');
+        $this->hugeProcessor->expects($this->once())
+                            ->method('persist')
+                            ->with(new PHPUnit_Framework_Constraint_Count(4));
 
+        $this->processor->persist($this->createQuadSet(4));
     }
 
+    /**
+     * Ensures that a quad set of the exact provided size is passed to the sub-processor
+     * that is responsible for small sets.
+     */
     public function testProcessorPassesSetsWithExactSizeToSmallProcessor()
     {
+        $this->smallProcessor->expects($this->once())
+                             ->method('persist')
+                             ->with(new PHPUnit_Framework_Constraint_Count(3));
+        $this->hugeProcessor->expects($this->never())
+                            ->method('persist');
 
+        $this->processor->persist($this->createQuadSet(3));
     }
 
+    /**
+     * Creates a quad set of the provided size.
+     *
+     * @param integer $size
+     * @return array(\Erfurt_Store_Adapter_Sparql_Quad)
+     */
     protected function createQuadSet($size)
     {
-
+        $quads = array();
+        for ($i = 0; $i < $size; $i++) {
+            $quads[] = new Erfurt_Store_Adapter_Sparql_Quad(
+                'http://example.org/subject' . $i,
+                'http://example.org/predicate' . $i,
+                array(
+                    'type'  => 'uri',
+                    'value' => 'http://example.org/object' . $i,
+                ),
+                'http://example.org/graph' . $i
+            );
+        }
+        return $quads;
     }
 
 }
