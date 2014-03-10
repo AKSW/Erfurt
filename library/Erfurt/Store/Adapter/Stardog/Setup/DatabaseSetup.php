aecadffc8e6062c12ff5a6314f464e60d006b9d3
Matthias Molitor <matthias@matthimatiker.de>
2014-03-10 12:32:51 +0100

    added class stub for database setup (#28)

---
+++ library/Erfurt/Store/Adapter/Stardog/Setup/DatabaseSetup.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Creates a Stardog database if not already available.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 10.03.14
+ */
+class Erfurt_Store_Adapter_Stardog_Setup_DatabaseSetup implements Erfurt_Store_Adapter_Container_SetupInterface
+{
+
+    /**
+     * Checks if the feature is already installed.
+     *
+     * @return boolean
+     */
+    public function isInstalled()
+    {
+        // TODO: Implement isInstalled() method.
+    }
+
+    /**
+     * Installs the feature.
+     */
+    public function install()
+    {
+        // TODO: Implement install() method.
+    }
+
+    /**
+     * Removes a previously installed feature.
+     *
+     * All stored data will be lost.
+     */
+    public function uninstall()
+    {
+        // TODO: Implement uninstall() method.
+    }
+
+}
