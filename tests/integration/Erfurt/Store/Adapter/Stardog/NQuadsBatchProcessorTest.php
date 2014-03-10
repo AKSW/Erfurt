211b8b82dde075cae7ce636b81abee4898c44402
Matthias Molitor <matthias@matthimatiker.de>
2014-03-10 19:31:19 +0100

    refactoring: extracted base test case (#28)

--- tests/integration/Erfurt/Store/Adapter/Stardog/NQuadsBatchProcessorTest.php
+++ tests/integration/Erfurt/Store/Adapter/Stardog/NQuadsBatchProcessorTest.php
@@ -7,150 +7,14 @@
  * @since 09.03.14
  * @group Stardog
  */
-class Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessorTest extends \PHPUnit_Framework_TestCase
+class Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessorTest
+    extends Erfurt_Store_Adapter_Stardog_AbstractBatchProcessorTestCase
 {
-
-    /**
-     * System under test.
-     *
-     * @var Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessor
-     */
-    protected $processor = null;
-
-    /**
-     * Test helper that is used to initialize the environment.
-     *
-     * @var \Erfurt_StardogTestHelper
-     */
-    protected $helper = null;
-
-    /**
-     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
-     */
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->helper    = new Erfurt_StardogTestHelper();
-        $client          = $this->helper->getDataAccessClient();
-        $this->processor = new Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessor($client);
-    }
-
-    /**
-     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
-     */
-    protected function tearDown()
-    {
-        $this->processor = null;
-        $this->helper->cleanUp();
-        $this->helper = null;
-        parent::tearDown();
-    }
-
-    /**
-     * Checks if the batch processor implements the required interface.
-     */
-    public function testImplementsInterface()
-    {
-        $this->assertInstanceOf('Erfurt_Store_Adapter_Sparql_BatchProcessorInterface', $this->processor);
-    }
-
-    /**
-     * Checks if persist() can handle an empty quad list.
-     */
-    public function testPersistCanHandleEmptyQuadList()
-    {
-        $this->setExpectedException(null);
-        $this->processor->persist(array());
-    }
-
-    /**
-     * Checks if persist() adds a singe quad.
-     */
-    public function testPersistAddsSingleQuad()
-    {
-        $quad = new Erfurt_Store_Adapter_Sparql_Quad(
-            'http://example.org/subject',
-            'http://example.org/predicate',
-            array(
-                'type'  => 'uri',
-                'value' => 'http://example.org/object'
-            ),
-            'http://example.org/graph'
-        );
-
-        $this->processor->persist(array($quad));
-
-        $this->assertNumberOfTriples(1);
-    }
-
-    /**
-     * Checks if persist() adds multiple quads.
-     */
-    public function testPersistAddsMultipleQuads()
-    {
-        $quads = array(
-            new Erfurt_Store_Adapter_Sparql_Quad(
-                'http://example.org/subject1',
-                'http://example.org/predicate1',
-                array(
-                    'type'  => 'uri',
-                    'value' => 'http://example.org/object1'
-                ),
-                'http://example.org/graph'
-            ),
-            new Erfurt_Store_Adapter_Sparql_Quad(
-                'http://example.org/subject2',
-                'http://example.org/predicate2',
-                array(
-                    'type'  => 'uri',
-                    'value' => 'http://example.org/object2'
-                ),
-                'http://example.org/graph'
-            ),
-        );
-
-        $this->processor->persist($quads);
-
-        $this->assertNumberOfTriples(2);
-    }
-
-    /**
-     * Ensures that persist() adds the triples to the correct graphs.
-     */
-    public function testPersistAssignsQuadsToCorrectGraphs()
-    {
-        // Use quads that are assigned to multiple graphs.
-        $quads = array(
-            new Erfurt_Store_Adapter_Sparql_Quad(
-                'http://example.org/subject1',
-                'http://example.org/predicate1',
-                array(
-                    'type'  => 'uri',
-                    'value' => 'http://example.org/object1'
-                ),
-                'http://example.org/graph1'
-            ),
-            new Erfurt_Store_Adapter_Sparql_Quad(
-                'http://example.org/subject2',
-                'http://example.org/predicate2',
-                array(
-                    'type'  => 'uri',
-                    'value' => 'http://example.org/object2'
-                ),
-                'http://example.org/graph2'
-            ),
-        );
-
-        $this->processor->persist($quads);
-
-        $this->assertNumberOfTriplesInGraph('http://example.org/graph1', 1);
-        $this->assertNumberOfTriplesInGraph('http://example.org/graph2', 1);
-    }
 
     /**
      * Checks if the batch processor encodes umlauts correctly.
      */
-    public function testPersistEncodeUmlautsCorrectly()
+    public function testPersistEncodesUmlautsCorrectly()
     {
         $quad = new Erfurt_Store_Adapter_Sparql_Quad(
             'http://example.org/subject',
@@ -169,39 +33,14 @@ class Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessorTest extends \PHPUnit_Fra
     }
 
     /**
-     * Asserts that the given graph contains the expected number of triples.
+     * Creates the batch processor that is used in the tests.
      *
-     * @param string $graph
-     * @param integer $expected
+     * @return Erfurt_Store_Adapter_Sparql_BatchProcessorInterface
      */
-    protected function assertNumberOfTriplesInGraph($graph, $expected)
+    protected function createProcessor()
     {
-        $query  = 'SELECT * FROM <' . $graph . '> WHERE { ?s ?p ?o . }';
-        $this->assertNumberOfRowsSelected($expected, $query);
-    }
-
-    /**
-     * Asserts that the provided SPARQL query selects the expected number of rows.
-     *
-     * @param integer $expected
-     * @param string $query
-     */
-    protected function assertNumberOfRowsSelected($expected, $query)
-    {
-        $result = $this->helper->getApiClient()->query(array('query' => $query));
-        $this->assertTrue(isset($result['results']['bindings']), 'Unexpected result structure.');
-        $numberOfTriples = count($result['results']['bindings']);
-        $this->assertEquals($expected, $numberOfTriples);
-    }
-
-    /**
-     * Asserts that the whole database contains the expected number of triples.
-     *
-     * @param integer $expected
-     */
-    protected function assertNumberOfTriples($expected)
-    {
-        $this->assertEquals($expected, $this->helper->getApiClient()->size());
+        $client = $this->helper->getDataAccessClient();
+        return new Erfurt_Store_Adapter_Stardog_NQuadsBatchProcessor($client);
     }
 
 }
