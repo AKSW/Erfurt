9c48caeef8308112aee1d276d626ad6a5783aea9
Matthias Molitor <matthias@matthimatiker.de>
2014-03-11 16:40:34 +0100

    implemented abstract decorator (#32)

--- library/Erfurt/Store/Adapter/Sparql/AbstractSparqlConnectorDecorator.php
+++ library/Erfurt/Store/Adapter/Sparql/AbstractSparqlConnectorDecorator.php
@@ -7,6 +7,148 @@
  * @since 11.03.14
  */
 abstract class Erfurt_Store_Adapter_Sparql_AbstractSparqlConnectorDecorator
+    implements Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
 {
+
+    /**
+     * The decorated connector.
+     *
+     * @var Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
+     */
+    protected $innerConnector = null;
+
+    /**
+     * Creates a decorator that wraps the provided connector.
+     *
+     * @param Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface $connector
+     */
+    public function __construct(Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface $connector)
+    {
+        $this->innerConnector = $connector;
+    }
+
+    /**
+     * Adds the provided triple to the data store.
+     *
+     * @param string $graphIri
+     * @param Erfurt_Store_Adapter_Sparql_Triple $triple
+     */
+    public function addTriple($graphIri, \Erfurt_Store_Adapter_Sparql_Triple $triple)
+    {
+        $this->innerConnector->addTriple($graphIri, $triple);
+    }
+
+    /**
+     * Executes the provided SPARQL query and returns its results.
+     *
+     * The results of an ASK query must be returned as boolean.
+     *
+     * If the query produces a result set, then it must be returned as array
+     * in extended format.
+     * The extended format each value contains additional information about
+     * its type and properties such as the language:
+     *
+     *     array(
+     *         'head' => array(
+     *             'vars' => array(
+     *                 // Contains the names of all variables that occur in the result set.
+     *                 'variable1',
+     *                 'variable2'
+     *             )
+     *         )
+     *         'results' => array(
+     *             'bindings' => array(
+     *                 // Contains one entry for each result set row.
+     *                 // Each entry contains the variable name as key and a set
+     *                 // of additional information as value:
+     *                 array(
+     *                     'variable1' => array(
+     *                         'value' => 'http://example.org',
+     *                         'type'  => 'uri'
+     *                     ),
+     *                     'variable2' => array(
+     *                         'value' => 'Hello world!',
+     *                         'type'  => 'literal'
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     )
+     *
+     * @param string $sparqlQuery
+     * @return array|boolean
+     */
+    public function query($sparqlQuery)
+    {
+        return $this->innerConnector->query($sparqlQuery);
+    }
+
+    /**
+     * Deletes all triples in the given graph that match the provided pattern.
+     *
+     * @param string $graphIri
+     * @param Erfurt_Store_Adapter_Sparql_TriplePattern $pattern
+     * @return integer The number of deleted triples.
+     */
+    public function deleteMatchingTriples($graphIri, Erfurt_Store_Adapter_Sparql_TriplePattern $pattern)
+    {
+        return $this->innerConnector->deleteMatchingTriples($graphIri, $pattern);
+    }
+
+    /**
+     * Accepts a callback function and processes it in batch mode.
+     *
+     * In batch mode the connector can decide to optimize the execution
+     * for example by delaying inserts or wrapping the whole task
+     * into a transaction.
+     *
+     * However, using the batch mode does *not* guarantee transactional
+     * behavior.
+     *
+     * The callback receives the connector itself as argument, which
+     * can be used to issue commands:
+     *
+     *     $connector->batch(function (\Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface $batchConnector) {
+     *         $batchConnector->addTriple(
+     *             'http://example.org',
+     *             new \Erfurt_Store_Adapter_Sparql_Triple(
+     *                 'http://example.org/subject1',
+     *                 'http://example.org/predicate1',
+     *                 'http://example.org/object1'
+     *             );
+     *         );
+     *         $batchConnector->addTriple(
+     *             'http://example.org',
+     *             new \Erfurt_Store_Adapter_Sparql_Triple(
+     *                 'http://example.org/subject2',
+     *                 'http://example.org/predicate2',
+     *                 'http://example.org/object2'
+     *             );
+     *         );
+     *     });
+     *
+     * Finally, the batch() method returns the result of the provided callback:
+     *
+     *     // Result contains 42.
+     *     $result = $connector->batch(function () {
+     *         return 42;
+     *     });
+     *
+     * @param mixed $callback A callback function.
+     * @return mixed
+     * @throws InvalidArgumentException If an invalid callback is passed.
+     */
+    public function batch($callback)
+    {
+        if (!is_callable($callback)) {
+            $message = 'Valid callback expected.';
+            throw new InvalidArgumentException($message);
+        }
+        $decorator = $this;
+        $wrappedCallback = function () use ($callback, $decorator) {
+            return call_user_func($callback, $decorator);
+        };
+        return $this->innerConnector->batch($wrappedCallback);
+    }
 
 }
