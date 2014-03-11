eb34e799d6e63693e1fce7f58cca30602572cfb2
Matthias Molitor <matthias@matthimatiker.de>
2014-03-11 16:15:00 +0100

    added class tub for abstract decorator plus tests (#32)

---
+++ library/Erfurt/Store/Adapter/Sparql/AbstractSparqlConnectorDecorator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Optional base class for connector decorators.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 11.03.14
+ */
+abstract class Erfurt_Store_Adapter_Sparql_AbstractSparqlConnectorDecorator
+{
+
+}
---
+++ tests/unit/Erfurt/Store/Adapter/Sparql/AbstractSparqlConnectorDecoratorTest.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Tests the abstract connector decorator.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 11.03.14
+ */
+class Erfurt_Store_Adapter_Sparql_AbstractSparqlConnectorDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * System under test.
+     *
+     * @var Erfurt_Store_Adapter_Sparql_AbstractSparqlConnectorDecorator
+     */
+    protected $decorator = null;
+
+    /**
+     * The (mocked) decorated connector.
+     *
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $innerConnector = null;
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->innerConnector = $this->getMock('Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface');
+        $this->decorator      = $this->getMockForAbstractClass(
+            'Erfurt_Store_Adapter_Sparql_AbstractSparqlConnectorDecorator',
+            array($this->innerConnector)
+        );
+    }
+
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->decorator      = null;
+        $this->innerConnector = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Checks if the decorator implements the connector interface.
+     */
+    public function testImplementsInterface()
+    {
+        $this->assertInstanceOf('Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface', $this->decorator);
+    }
+
+    /**
+     * Checks if the decorator delegates the method calls to the inner decorator.
+     *
+     * @param string $method
+     * @param array(mixed) $arguments
+     * @param mixed $returnValue
+     * @dataProvider methodCallProvider
+     */
+    public function testDelegatesMethodCall($method, array $arguments, $returnValue)
+    {
+        $message = 'Decorator does not provide method "' . $method . '".';
+        $this->assertTrue(method_exists($this->decorator, $method), $message);
+
+        $this->innerConnector->expects($this->once())
+                             ->method($method)
+                             ->with($arguments)
+                             ->will($this->returnValue($returnValue));
+
+        $result = call_user_func_array(array($this->decorator, $method), $arguments);
+        $this->assertEquals($returnValue, $result);
+    }
+
+    /**
+     * Provides test data for each method call that must be delegated.
+     *
+     * Each record contains:
+     *
+     * - The name of the method.
+     * - A list of arguments.
+     * - The return value.
+     *
+     * @return array(array(string|array(mixed)|mixed))
+     */
+    public function methodCallProvider()
+    {
+        return array(
+            array(
+                'addTriple',
+                array(
+                    'http://example.org/graph',
+                    new Erfurt_Store_Adapter_Sparql_Triple(
+                        'http://example.org/subject',
+                        'http://example.org/predicate',
+                        array('type' => 'uri', 'value' => 'http://example.org/object')
+                    )
+                ),
+                null
+            ),
+            array(
+                'query',
+                array(
+                    'ASK WHERE { ?s ?p ?o . }'
+                ),
+                true
+            ),
+            array(
+                'deleteMatchingTriples',
+                array(
+                    'http://example.org/graph',
+                    new Erfurt_Store_Adapter_Sparql_TriplePattern(null, null, null)
+                ),
+                42
+            ),
+            array(
+                'batch',
+                array(
+                    function () {}
+                ),
+                null
+            )
+        );
+    }
+
+}
