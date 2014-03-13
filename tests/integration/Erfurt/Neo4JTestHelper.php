16d3c6c3b74aa859c6a2b0116b3f958b58bf1181
Matthias Molitor <matthias@matthimatiker.de>
2014-03-13 16:56:14 +0100

    created test helper stub (#42)

---
+++ tests/integration/Erfurt/Neo4JTestHelper.php
@@ -0,0 +1,90 @@
+<?php
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Helper class that is used to set up the environment for Neo4J integration tests.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 13.03.14
+ */
+class Erfurt_Neo4JTestHelper extends Erfurt_AbstractTestHelper
+    implements \Erfurt_Store_Adapter_Sparql_ConnectorBenchmarkHelperInterface
+{
+
+    /**
+     * The API client instance that has been created.
+     *
+     * @var Erfurt_Store_Adapter_Neo4J_ApiClient|null
+     */
+    protected $apiClient = null;
+
+    /**
+     * The service container or null if it was not initialized yet.
+     *
+     * @var ContainerInterface|null
+     */
+    protected $container = null;
+
+    /**
+     * Returns a SPARQL connector, which is used in the benchmark and that operates
+     * on a fresh (= empty) triple store database.
+     *
+     * @return \Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface
+     * @throws BadMethodCallException Currently not implemented.
+     */
+    public function getSparqlConnector()
+    {
+        throw new BadMethodCallException('Not implemented yet.');
+    }
+
+    /**
+     * Returns the Neo4J API client instance.
+     *
+     * @return Erfurt_Store_Adapter_Neo4J_ApiClient
+     */
+    public function getApiClient()
+    {
+        if ($this->apiClient === null) {
+            $container = $this->getContainer();
+            $client    = $container->get('neo4j.api_client');
+            PHPUnit_Framework_Assert::assertInstanceOf('Erfurt_Store_Adapter_Neo4J_ApiClient', $client);
+            $this->apiClient = $client;
+            $this->addCleanUpTask(array($this, 'unsetApiClient'));
+        }
+        return $this->apiClient;
+    }
+
+    /**
+     * Returns the service container that wires the different
+     * components for the Stardog connector.
+     *
+     * @return ContainerInterface
+     */
+    public function getContainer()
+    {
+        if ($this->container === null) {
+            $config = $this->getConfig();
+            $config['service'] = 'service_container';
+            $this->container = \Erfurt_Store_Adapter_Container::createFromOptions($config);
+            $this->addCleanUpTask(array($this, 'unsetContainer'));
+        }
+        return $this->container;
+    }
+
+    /**
+     * Destroys the created API client.
+     */
+    protected function unsetApiClient()
+    {
+        $this->apiClient = null;
+    }
+
+    /**
+     * Destroys the created service container.
+     */
+    protected function unsetContainer()
+    {
+        $this->container = null;
+    }
+
+}
