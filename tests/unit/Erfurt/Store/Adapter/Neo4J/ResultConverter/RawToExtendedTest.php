5ce55c6242dc99de11f449aed103e59e551bbdf9
Matthias Molitor <matthias@matthimatiker.de>
2014-03-15 17:27:34 +0100

    added test case stub (#42)

---
+++ tests/unit/Erfurt/Store/Adapter/Neo4J/ResultConverter/RawToExtendedTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Tests the result converter.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 15.03.14
+ */
+class Erfurt_Store_Adapter_Neo4J_ResultConverter_RawToExtendedTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterface()
+    {
+
+    }
+
+    public function testThrowsExceptionIfNoArrayIsProvided()
+    {
+
+    }
+
+    public function testConverterTransformsUrisCorrectly()
+    {
+
+    }
+
+    public function testConverterTransformsSimpleLiteralsCorrectly()
+    {
+
+    }
+
+    public function testConverterTransformsTypedLiteralsCorrectly()
+    {
+
+    }
+
+    public function testConverterTransformsLiteralsWithLanguageCorrectly()
+    {
+        
+    }
+
+}
