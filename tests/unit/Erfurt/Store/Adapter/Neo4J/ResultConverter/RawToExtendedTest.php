6a9ee640c6037060314fdf827488b49f0343fd7a
Matthias Molitor <matthias@matthimatiker.de>
2014-03-15 17:33:36 +0100

    added test stubs (#42)

--- tests/unit/Erfurt/Store/Adapter/Neo4J/ResultConverter/RawToExtendedTest.php
+++ tests/unit/Erfurt/Store/Adapter/Neo4J/ResultConverter/RawToExtendedTest.php
@@ -9,36 +9,58 @@
 class Erfurt_Store_Adapter_Neo4J_ResultConverter_RawToExtendedTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Checks if the converter implements the required interface.
+     */
     public function testImplementsInterface()
     {
 
     }
 
+    /**
+     * Ensures that convert() throws an exception if no array is passed
+     * as input result set.
+     */
     public function testThrowsExceptionIfNoArrayIsProvided()
     {
 
     }
 
+    /**
+     * Checks if convert() can handle an empty input result set.
+     */
     public function testConverterCanHandleEmptySet()
     {
 
     }
 
+    /**
+     * Checks if URIs are transformed correctly.
+     */
     public function testConverterTransformsUrisCorrectly()
     {
 
     }
 
+    /**
+     * Checks if simple literals are converted correctly.
+     */
     public function testConverterTransformsSimpleLiteralsCorrectly()
     {
 
     }
 
+    /**
+     * Checks if typed literals are transformed correctly.
+     */
     public function testConverterTransformsTypedLiteralsCorrectly()
     {
 
     }
 
+    /**
+     * Checks if literals with language information are converted correctly.
+     */
     public function testConverterTransformsLiteralsWithLanguageCorrectly()
     {
 
