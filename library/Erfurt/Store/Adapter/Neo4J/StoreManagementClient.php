758427d19fc27958a32a195822ce224730d1a57b
Matthias Molitor <matthias@matthimatiker.de>
2014-03-16 21:04:56 +0100

    fixed value assignment: raw literal value must be assigned to object node (#42)

--- library/Erfurt/Store/Adapter/Neo4J/StoreManagementClient.php
+++ library/Erfurt/Store/Adapter/Neo4J/StoreManagementClient.php
@@ -50,7 +50,7 @@ class Erfurt_Store_Adapter_Neo4J_StoreManagementClient
             'predicateP'    => 'U ' . $triple->getPredicate(),
             'predicateCP'   => 'U ' . $graphUri . ' U ' . $triple->getPredicate(),
             'objectTerm'    => $triple->format('?object'),
-            'objectValue'   => (($object['type'] === 'literal') ? $triple->format('?object') : $object['value']),
+            'objectValue'   => $object['value'],
             'objectKind'    => $object['type']
         );
 
