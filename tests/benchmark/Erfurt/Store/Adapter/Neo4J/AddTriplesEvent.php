f8faa3e3330daaac8374a16cea613a7488061e08
Matthias Molitor <matthias@matthimatiker.de>
2014-03-18 19:58:06 +0100

    added benchmark classes (#44)

---
+++ tests/benchmark/Erfurt/Store/Adapter/Neo4J/AddTriplesEvent.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * See {@link \Erfurt_Store_Adapter_Sparql_AbstractAddTriplesAthleticEvent} for details.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 18.03.14
+ */
+class Erfurt_Store_Adapter_Neo4J_AddTriplesEvent
+    extends Erfurt_Store_Adapter_Sparql_AbstractAddTriplesAthleticEvent
+{
+
+}
