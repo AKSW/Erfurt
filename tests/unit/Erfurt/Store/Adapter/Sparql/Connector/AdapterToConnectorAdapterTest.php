fd2fe7346fdae5664ce6dbc657cd93bfc445427a
Matthias Molitor <matthias@matthimatiker.de>
2014-04-28 17:00:39 +0200

    fixed test data (#47)

--- tests/unit/Erfurt/Store/Adapter/Sparql/Connector/AdapterToConnectorAdapterTest.php
+++ tests/unit/Erfurt/Store/Adapter/Sparql/Connector/AdapterToConnectorAdapterTest.php
@@ -111,8 +111,8 @@ class Erfurt_Store_Adapter_Sparql_Connector_AdapterToConnectorAdapterTest extend
                 $connector
             );
             /* @var $connector Erfurt_Store_Adapter_Sparql_SparqlConnectorInterface */
-            $connector->addTriple('http://example.org', $first);
-            $connector->addTriple('http://example.org', $second);
+            $connector->addTriple('http://example.org/graph', $first);
+            $connector->addTriple('http://example.org/graph', $second);
         });
     }
 
@@ -121,7 +121,7 @@ class Erfurt_Store_Adapter_Sparql_Connector_AdapterToConnectorAdapterTest extend
      */
     public function testQueryDelegatesToSparqlQuery()
     {
-        $query = 'SELECT * WHERE { ?s ?p ? o . }';
+        $query = 'SELECT * WHERE { ?s ?p ?o . }';
         $this->storeAdapter->expects($this->once())
                            ->method('sparqlQuery')
                            ->with($query, array());
@@ -134,7 +134,7 @@ class Erfurt_Store_Adapter_Sparql_Connector_AdapterToConnectorAdapterTest extend
      */
     public function testQueryDelegatesAskQueryToSparqlAsk()
     {
-        $query = 'ASK WHERE { ?s ?p ? o . }';
+        $query = 'ASK WHERE { ?s ?p ?o . }';
         $this->storeAdapter->expects($this->once())
                            ->method('sparqlAsk')
                            ->with($query);
