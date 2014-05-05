16ae42cd98141d7bacef75ed93074d4eed475624
Matthias Molitor <matthias@matthimatiker.de>
2014-05-05 18:25:35 +0200

    created MySQL/Virtuoso benchmarks that use 1% of the data set (#51)

---
+++ tests/benchmark/Erfurt/Store/Adapter/Virtuoso/DBpedia1PercentBenchmarkEvent.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * See {@link \Erfurt_Store_Adapter_Sparql_AbstractDBpediaBenchmarkAthleticEvent} for details.
+ *
+ * @author Matthias Molitor <molitor@informatik.uni-bonn.de>
+ * @since 30.04.14
+ */
+class Erfurt_Store_Adapter_Virtuoso_DBpedia1PercentBenchmarkEvent
+    extends Erfurt_Store_Adapter_Sparql_AbstractDBpediaBenchmarkAthleticEvent
+{
+
+    /**
+     * Creates the benchmark event.
+     */
+    public function __construct()
+    {
+        parent::__construct(1);
+    }
+
+}
