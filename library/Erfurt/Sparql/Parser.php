c2764ee7049ec8d5cfb5eeaa34517230018d5e73
Gregor Tätzner <brummbq@gmail.com>
2015-10-26 17:28:07 +0100

    * allow less operator in filter clauses and don't switch to uri mode there

--- library/Erfurt/Sparql/Parser.php
+++ library/Erfurt/Sparql/Parser.php
@@ -112,7 +112,7 @@ class Erfurt_Sparql_Parser
         
         $inLiteral   = false;
         $longLiteral = false;
-        
+
         for ($i=0; $i<$len; ++$i) {
             if (in_array($queryString[$i], $removeableSpecialChars) && !$inLiteral && !$inTelUri) {        
                 if (isset($tokens[$n]) && $tokens[$n] !== '') {
@@ -224,15 +224,19 @@ class Erfurt_Sparql_Parser
                     $inUri = false;
                     continue;
                 } else if ($queryString[$i] === '<') {
-                    $inUri = true;
-                    if ($tokens[$n] === '') {
-                        $tokens[$n] = '<';
-                        continue;
-                    } else {
-                        $tokens[++$n] = '<';
-                        continue;
-                    }
+                    // this is a less operator, if we are in a filter clause and this is the second argument and also followed by a whitespace
+                    if (isset($tokens[$n-3]) && $tokens[$n-3] !== 'FILTER' && isset($queryString[$i+1]) && $queryString[$i+1] !== ' ') {
+                        // this is an uri
+                        $inUri = true;
 
+                        if ($tokens[$n] === '') {
+                            $tokens[$n] = '<';
+                            continue;
+                        } else {
+                            $tokens[++$n] = '<';
+                            continue;
+                        }
+                    }
                 }
                  
                 $tokens[$n] .= $queryString{$i};
